99fcad023ee2ba54adb7f6b755b891827f17a36d
Fredi Kats <fredikats@outlook.com>
2017-05-03 07:44:45 +0300

    Huge update v0.0.2
    Реализовано все, что планировалось. А потом еще что-то напланировалось.
    Так что следующий комит тоже будет реализовывать все, что планируется.

---
+++ PhisProject/PhysicalCanvas.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using PhysProject.Source;
+
+namespace PhysProject
+{
+    public partial class MainWindow : Window
+    {
+        private PhysicalField _field;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _field = new PhysicalField(MainCanvas, Config.TimePerTick);
+            _field.AddObject(new TwoDimesional(10, 10), new TwoDimesional(50, 50),
+                new TwoDimesional(0, 0));
+            ButtonStart.Click += _field.Start;
+        }
+    }
+}
---
+++ PhisProject/TestingTool/CustomObject.cs
@@ -0,0 +1,21 @@
+﻿using PhysProject.Source;
+
+namespace PhysProject.TestingTool
+{
+    public class CustomObject : PhysicalObject
+    {
+        public CustomObject(PhysicalField field, TwoDimesional size, TwoDimesional position, TwoDimesional moveDirection) : base(field, size, position, moveDirection)
+        {
+        }
+
+        protected override void SetAccelerationVector()
+        {
+            if (Position.Y >= 290)
+            {
+                StopMoving();
+                return;
+            }
+            AccelerationDirection = new TwoDimesional(10, 7);
+        }
+    }
+}
