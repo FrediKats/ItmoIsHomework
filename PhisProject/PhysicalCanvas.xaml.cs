6f23968126e5461562aae58c900a9ccf411e9861
Inredika WhiteBlack <inredikawb@gmail.com>
2017-05-13 04:21:24 +0300

    Update v0.0.6
    For testing in projects

--- PhisProject/PhysicalCanvas.xaml.cs
+++ PhisProject/PhysicalCanvas.xaml.cs
@@ -13,25 +13,7 @@ namespace PhysProject
         {
 
             InitializeComponent();
-
-            _field = new PhysicalField(MainCanvas, Config.TimePerTick);
-            _field.AddObject(new CustomObject(_field, 10,
-                new TwoDimesional(50, 50), new TwoDimesional(10, 0)));
-            _field.AddObject(new CustomObject(_field, 10,
-                new TwoDimesional(80, 80), new TwoDimesional(0, -10)));
-            
-            ButtonStart.Click += _field.Start;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            double v = double.Parse(TextBoxV.Text);
-            double a = double.Parse(TextBoxA.Text) / 180 * Math.PI;
-
-            double vx = v * Math.Cos(a);
-            double vy = v * Math.Sin(a);
-            _field.AddObject(new Balist(_field, 10,
-                new TwoDimesional(100, int.Parse(TextBoxH.Text)+5), new TwoDimesional(vx, vy)));
+            UpdateUserInterface();
         }
     }
 }
--- PhisProject/Source/Tool.cs
+++ PhisProject/Source/Tool.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace PhysProject.Source
 {
@@ -7,6 +9,18 @@ namespace PhysProject.Source
         public static double Distance(TwoDimesional f, TwoDimesional s)
         {
             return  Math.Sqrt(Math.Pow(f.X - s.X, 2) + Math.Pow(f.Y - s.Y, 2));
+        }
+
+        public static Ellipse GenerateEllipse(double size)
+        {
+            return new Ellipse()
+            {
+                Height = size,
+                Width = size,
+                StrokeThickness = 1,
+                Stroke = new SolidColorBrush() { Color = Colors.Navy },
+                Fill = new SolidColorBrush() { Color = Colors.Yellow }
+            };
         }
     }
 }
