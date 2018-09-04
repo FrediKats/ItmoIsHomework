fcaa7f7ec5429a3da477c6c444048f332c7a993a
Unknown <sheremet5555@gmail.com>
2018-09-05 01:10:10 +0300

    Lab 1, part 1, methods 1&2

---
+++ OptimizationMethods/MinimumSearch.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    public static class MinimumSearch
+    {
+        public static double BinarySearch(Func<double, double> function, double left, double right, double epsilon)
+        {
+            double delta = 0.8 * epsilon / 2;
+
+            while (right - left >= epsilon)
+            {
+                double x1 = (left + right) / 2 - delta;
+                double x2 = (left + right) / 2 + delta;
+
+                double f1 = function(x1);
+                double f2 = function(x2);
+
+                if (f1 <= f2)
+                {
+                    right = x2;
+                }
+
+                if (f1 >= f2)
+                {
+                    left = x1;
+                }
+            }
+
+            return (right + left) / 2;
+        }
+
+        public static double GoldenRatio(Func<double, double> function, double left, double right, double epsilon)
+        {
+            double x1 = default;
+            double x2 = default;
+            double f1 = default;
+            double f2 = default;
+
+            if (right - left >= epsilon)
+            {
+                x1 = left + (right - left) * (3 - Math.Sqrt(5)) / 2;
+                x2 = left + (right - left) * (Math.Sqrt(5) - 1) / 2;
+
+                f1 = function(x1);
+                f2 = function(x2);
+
+                if (f1 >= f2)
+                {
+                    left = x1;
+                }
+                else
+                {
+                    right = x2;
+                }
+            }
+
+            while (right - left >= epsilon)
+            {
+                if (x1 == left)
+                {
+                    x1 = x2;
+                    f1 = f2;
+                    x2 = left + (right - left) * (Math.Sqrt(5) - 1) / 2;
+                    f2 = function(x2);
+                }
+                else
+                {
+                    x2 = x1;
+                    f2 = f1;
+                    x1 = left + (right - left) * (3 - Math.Sqrt(5)) / 2;
+                    f1 = function(x1);
+                }
+
+                if (f1 >= f2)
+                {
+                    left = x1;
+                }
+                else
+                {
+                    right = x2;
+                }
+            }
+
+            return (right + left) / 2;
+        }
+
+        public static double Fibonacci(Func<double, double> function, double left, double right, double epsilon)
+        {
+
+
+            return (right - left) / 2;
+        }
+    }
+}
--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace OptimizationMethods
+namespace Lab1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MinimumSearch.BinarySearch(x => Math.Pow(x - 5, 2), -1, 6, 0.001));
+            Console.WriteLine(MinimumSearch.GoldenRatio(x => Math.Pow(x - 5, 2), -1, 6, 0.001));
         }
     }
 }
