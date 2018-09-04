ee2c3b574050d1a436518a298ca800dd5f71063a
Sergei <sheremet5555@gmail.com>
2018-09-04 19:58:28 +0300

    Add project files.

---
+++ OptimizationMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OptimizationMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
