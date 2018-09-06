367cca4005543c794e67ce75f653de98886f6580
Fredi Kats <fredikats@outlook.com>
2018-09-06 23:43:31 +0300

    CountableFunction

---
+++ OptimizationMethods/Tools/CountableFunc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab1.Tools
+{
+    public class CountableFunc
+    {
+        public CountableFunc(Func<double, double> function, double left, double right, double epsilon)
+        {
+            _function = function;
+            Left = left;
+            Right = right;
+            Epsilon = epsilon;
+
+            _callCount = 0;
+        }
+
+        private readonly Func<double, double> _function;
+        private int _callCount;
+
+        public Func<double, double> Function
+        {
+            get
+            {
+                _callCount++;
+                return _function;
+            }
+        }
+
+        public int CallCount => _callCount;
+        public double Left { get; }
+        public double Right { get; }
+        public double Epsilon { get; }
+
+    }
+}
---
+++ OptimizationMethods/Tools/CountableMultiDimentionalFunc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1.Tools
+{
+    public class CountableMultiDimensionalFunc
+    {
+        public CountableMultiDimensionalFunc(ScalarField<double> function, List<double> startPoint, double functionEpsilon, List<double> parameterEpsilon)
+        {
+            _function = function;
+            StartPoint = startPoint;
+            FunctionEpsilon = functionEpsilon;
+            ParameterEpsilon = parameterEpsilon;
+        }
+
+        private readonly ScalarField<double> _function;
+        private int _callCount;
+
+        public ScalarField<double> Function
+        {
+            get
+            {
+                _callCount++;
+                return _function;
+            }
+        }
+
+        public int CallCount => _callCount;
+        public List<double> StartPoint { get; }
+        public double FunctionEpsilon { get; }
+        public List<double> ParameterEpsilon { get; }
+    }
+}
