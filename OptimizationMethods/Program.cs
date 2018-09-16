a4ac3b98ef8194154affcb55807cabb7954bbcf7
Unknown <maryshipkova@gmail.com>
2018-09-16 15:23:38 +0300

    multi dimentional tests

--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -9,11 +9,11 @@ namespace Lab1
 {
     internal class Program
     {
-        private static void testing(){
+        private static void oneDimentionalTesting(){
             Console.WriteLine("Test 1: f (x) = sin (x), x ∈ [-pi/2, pi/2]");
-            Console.WriteLine("Expected: 0");
+            Console.WriteLine("Expected: -pi/2");
             Console.WriteLine("Result:");
-            printResult(x => Math.Sin(x), -Math.PI/2, Math.PI/2);
+            printOneDimentionalResult(x => Math.Sin(x), -Math.PI/2, Math.PI/2);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -21,7 +21,7 @@ namespace Lab1
             Console.WriteLine("Test 2: f (x) = cos (x), x ∈ [0, pi]");
             Console.WriteLine("Expected: pi");
             Console.WriteLine("Result:");
-            printResult(x => Math.Cos(x), 0, Math.PI);
+            printOneDimentionalResult(x => Math.Cos(x), 0, Math.PI);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -29,7 +29,7 @@ namespace Lab1
             Console.WriteLine("Test 3: (x - 2)^2, x ∈ [-2, 20]");
             Console.WriteLine("Expected: 2");
             Console.WriteLine("Result:");
-            printResult(x => Math.Pow(x-2, 2), -2, 20);
+            printOneDimentionalResult(x => Math.Pow(x-2, 2), -2, 20);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -37,7 +37,7 @@ namespace Lab1
             Console.WriteLine("Test 4: (x - 15)^2 + 5, x ∈ [2, 200]");
             Console.WriteLine("Expected: 15");
             Console.WriteLine("Result:");
-            printResult(x => Math.Pow(x-15, 2)+5, 2, 200);
+            printOneDimentionalResult(x => Math.Pow(x-15, 2)+5, 2, 200);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -45,7 +45,7 @@ namespace Lab1
             Console.WriteLine("Test 5: (x + 5)^4, x ∈ [-10, 15]");
             Console.WriteLine("Expected: -5");
             Console.WriteLine("Result:");
-            printResult(x => Math.Pow(x+5, 4), -10, 15);
+            printOneDimentionalResult(x => Math.Pow(x+5, 4), -10, 15);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -53,7 +53,7 @@ namespace Lab1
             Console.WriteLine("Test 6: exp (x), x ∈ [0, 100]");
             Console.WriteLine("Expected: 0");
             Console.WriteLine("Result:");
-            printResult(x => Math.Exp(x), 0, 100);
+            printOneDimentionalResult(x => Math.Exp(x), 0, 100);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -61,7 +61,7 @@ namespace Lab1
             Console.WriteLine("Test 7: x^2 + 2x - 4, x ∈ [-10, 20]");
             Console.WriteLine("Expected: -1");
             Console.WriteLine("Result:");
-            printResult(x => Math.Pow(x, 2)+2*x-4, -10, 20);
+            printOneDimentionalResult(x => Math.Pow(x, 2)+2*x-4, -10, 20);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
@@ -69,19 +69,72 @@ namespace Lab1
             Console.WriteLine("Test 8: x^3 - x, x ∈ [0, 1]");
             Console.WriteLine("Expected: ~0,57-0,58");
             Console.WriteLine("Result:");
-            printResult(x =>  Math.Pow(x, 3)-x, 0, 1);
+            printOneDimentionalResult(x =>  Math.Pow(x, 3)-x, 0, 1);
             Console.WriteLine();
             Console.WriteLine("-------------------------");
             Console.WriteLine();
 
         }
-         private static void printResult(Func<double, double> fun, double min, double max ){
+         private static void printOneDimentionalResult(Func<double, double> fun, double min, double max ){
             Console.WriteLine("BinarySearch:   " + MinimumSearch.BinarySearch(new CountableFunc(fun, min, max , 0.001)));
             Console.WriteLine("GoldenRatio:   " + MinimumSearch.GoldenRatio(new CountableFunc(fun, min, max, 0.001)));
             Console.WriteLine("FibonacciMethod:   " + MinimumSearch.FibonacciMethod(new CountableFunc(fun, min, max, 0.001)));
             Console.WriteLine("DirectSearch:   " + MinimumSearch.DirectSearch(new CountableFunc(fun, min, max, 0.001)));
         }
 
+        private static void multiDimentionalTesting(){
+            Console.WriteLine("Test 1: f(x) = 100(x2 - x1^2)^2 + (1 - x1)^2");
+            Console.WriteLine("Expected: x1 = 1, x2 = 1");
+            Console.WriteLine("Result:");
+            printMultiDimentionalResult( x => 100 * Math.Pow(x[1] - Math.Pow(x[0], 2), 2) + Math.Pow(1 - x[0], 2));
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Test 2:  f(x) = (x2 - x1^2)^2 + (1 - x1)^2");
+            Console.WriteLine("Expected: x1 = 1, x2 = 1");
+            Console.WriteLine("Result:");
+            printMultiDimentionalResult( x => Math.Pow(x[1] - Math.Pow(x[0], 2), 2) + Math.Pow(1 - x[0], 2));
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Test 3: f(x) = (x2 - x1)^2 + 100(1 - x1)^2");
+            Console.WriteLine("Expected: x1 = 1, x2 = 1");
+            Console.WriteLine("Result:");
+            printMultiDimentionalResult( x => 100 * Math.Pow(x[1] -x[0], 2) + 100*Math.Pow(1 - x[0], 2));
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Test 4: f(x) = 100(x2 - x1^3)^2 + (1 - x1)^2");
+            Console.WriteLine("Expected: x1 = 1, x2 = 1");
+            Console.WriteLine("Result:");
+            printMultiDimentionalResult( x => 100 * Math.Pow(x[1] - Math.Pow(x[0], 3), 2) + Math.Pow(1 - x[0], 2));
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Test 5: f(x) =  (1.5 - x(1 - y))^2 + (2.25 - x(1 - y^2))^2 + (2.625 - x(1 - y^3))^2");
+            Console.WriteLine("Expected: x = 3, y = .5");
+            Console.WriteLine("Result:");
+            printMultiDimentionalResult( x => Math.Pow(1.5 - x[0]*(1-x[1]), 2) +  Math.Pow(2.25 - x[0]*(1-Math.Pow(x[1],2)), 2)+ Math.Pow(2.625 - x[0]*(1-Math.Pow(x[1],3)), 2));
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+        }
+
+         private static void printMultiDimentionalResult(Func<Dimensions, double> fun ){
+            MultidimensionalMinimumSearch.GradientDescent(
+                   new CountableMultiDimensionalFunc(fun,
+                       new Dimensions(new[] {1.0, 5.0}),
+                       0.1,
+                       new Dimensions(new[] {1.0, 1.0})))
+               .Coords
+               .ToList()
+               .ForEach(Console.WriteLine);
+        }
         private static void Main(string[] args)
         {
             Func<double, double> func = x => Math.Pow(x - 5, 2);
@@ -123,10 +176,11 @@ namespace Lab1
                 new Dimensions(new[] { 0.1, 0.1, 0.1, 1.0 })
                 );
 
-            LabLogger.ExecuteFirstTaskGradient(new List<CountableMultiDimensionalFunc> {f0, f1, f7}, "gradient.xlsx" );
+            // LabLogger.ExecuteFirstTaskGradient(new List<CountableMultiDimensionalFunc> {f0, f1, f7}, "gradient.xlsx" );
 
-            testing();
-            //MultidimensionalMinimumSearch.GradientDescent(
+            // oneDimentionalTesting();
+            multiDimentionalTesting();
+            // MultidimensionalMinimumSearch.GradientDescent(
             //        new CountableMultiDimensionalFunc(field,
             //            new Dimensions(new[] {1.0, 5.0, 2.0}),
             //            0.1,
