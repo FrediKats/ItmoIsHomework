c37e327caf3fb62f9bbaa1bb42503ef30794eec1
Unknown <sheremet5555@gmail.com>
2018-09-27 01:31:02 +0300

    The beginning of Lab 2

---
+++ Lab2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+
+            double[,] matrix =
+            {
+                { 0, 5, 2, 7, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 6, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 5, 7, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 4, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 0, 0, 0, 9, 5, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 9 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 7 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+
+            var graph = new Graph(matrix);
+
+            graph.ShortestPath(0, graph.Order - 1);
+        }
+    }
+}
