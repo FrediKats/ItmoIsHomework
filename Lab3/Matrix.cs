38f76372a91d02132c1f4bf89d6ec88ed14af284
Unknown <sheremet5555@gmail.com>
2018-10-18 23:18:42 +0300

    Diagonal matrix

--- Lab3/Matrix.cs
+++ Lab3/Matrix.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Lab3
 {
     class Matrix
     {
-        private double[,] _data;
+        private double[][] _data;
 
-        public double[,] Data => _data; //TODO: rewrite
+        //public double[][] Data => _data; //TODO: rewrite or delete
 
-        public Matrix(double[,] data)
+        public Matrix(double[][] data)
         {
             _data = data;
         }
 
         public void DiagonalForm()
         {
-            if (Data.GetLength(0) >= Data.GetLength(1))
+            //write more exceptions
+
+            if (_data.Select(x => x[0]).All(x => x == 0))
             {
-                throw new ArgumentException();
+                throw new Exception();
+            }
+
+            _data = _data.OrderByDescending(x => Math.Abs(x[0])).ToArray();
+
+            for (int i = 0; i < _data.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    _data[i] = _data[i].Zip(_data[j], (curr, prev) => curr - prev * _data[i][j]).ToArray();
+                }
+
+                _data[i] = _data[i].Select(x => x / _data[i][i]).ToArray();
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    _data[j] = _data[j].Zip(_data[i], (curr, prev) => curr - prev * _data[j][i]).ToArray();
+                }
             }
         }
     }
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -7,17 +7,19 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            double[,] A = 
+            double[][] A = 
             {
-                { 1, 2, -1, 2, 4 },
-                { 0, -1, 2, 1, 3 },
-                { 1, -3, 2, 2, 0 }
+                new double[] { 1, 2, -1, 2, 4 },
+                new double[] { 0, -1, 2, 1, 3 },
+                new double[] { 1, -3, 2, 2, 0 }
             };
 
             double[] b = { 1, 3, 4 };
             double[] c = { 1, -3, 2, 1, 4 };
 
-            double[,] simplexTable = new double[A.GetLength(0) + 1, A.GetLength(1) - A.GetLength(0) + 1];
+            //double[,] simplexTable = new double[A.GetLength(0) + 1, A.GetLength(1) - A.GetLength(0) + 1];
+            Matrix m = new Matrix(A);
+            m.DiagonalForm();
         }
     }
 }
