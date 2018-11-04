c8651c12e8e1de5425e83e54195c98ce56165aec
Fredi Kats <fredikats@outlook.com>
2018-11-04 14:06:01 +0300

    Basic generic models

---
+++ GeneticWay.Genetic/Models/Gene.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GeneticWay.Genetic.Models
+{
+    [DebuggerDisplay("{Value}")]
+    public struct Gene<T>
+    {
+        public T Value { get; }
+
+        //TODO: remove func for memory optimization
+        private readonly Func<Gene<T>, Gene<T>> _mutationFunc;
+
+        public Gene(T value, Func<Gene<T>, Gene<T>> mutationFuncFunc)
+        {
+            Value = value;
+            _mutationFunc = mutationFuncFunc;
+        }
+
+        public Gene<T> CreateMutation()
+        {
+            return _mutationFunc(this);
+        }
+    }
+}
---
+++ GeneticWay.Genetic/Tools/ExceptionTools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeneticWay.Genetic.Tools
+{
+    public static class ExceptionTools
+    {
+        public static void ThrowIfNull(string argumentName, object argument)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+    }
+}
