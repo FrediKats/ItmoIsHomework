4302b76eb217d20e94befb301782c32123dc2b60
Fredi Kats <fredikats@outlook.com>
2018-11-08 18:37:23 +0300

    Add selection algorithms

---
+++ GeneticWay.Genetic/Selections/TournamentSelection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeneticWay.Genetic.Models;
+using GeneticWay.Genetic.Tools.Randomization;
+
+namespace GeneticWay.Genetic.Selections
+{
+    public class TournamentSelection : ISelection
+    {
+        public TournamentSelection(bool isWinnerContinue, int genotypeInRound)
+        {
+            IsWinnerContinue = isWinnerContinue;
+            GenotypeInRound = genotypeInRound;
+        }
+
+        public bool IsWinnerContinue { get; }
+        public int GenotypeInRound { get; }
+
+        public List<BaseGenotype<T>> SelectChromosomes<T>(int number, IList<BaseGenotype<T>> generation)
+        {
+            List<BaseGenotype<T>> genotypesPool = generation.ToList();
+            var selectedList = new List<BaseGenotype<T>>(number);
+
+            for (int iteration = 0; iteration < number; iteration++)
+            {
+                int[] randomIndexes = RandomizationProvider.Current.GetUniqueInts(GenotypeInRound, 0, genotypesPool.Count);
+                BaseGenotype<T> tournamentWinner = genotypesPool
+                    .Where((c, i) => randomIndexes.Contains(i))
+                    .OrderByDescending(c => c.Fitness)
+                    .First();
+
+                selectedList.Add(tournamentWinner);
+
+                if (!IsWinnerContinue)
+                {
+                    genotypesPool.Remove(tournamentWinner);
+                }
+            }
+
+            return selectedList;
+        }
+    }
+}
