218f9c44542a12917e057cdceca87c96cebd2f8d
Fredi Fowler <inredikawb@gmail.com>
2018-11-20 09:25:30 +0300

    Add Mvvm command implementation

---
+++ SubjectSolutionManager/Tools/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SubjectSolutionManager.Tools
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
