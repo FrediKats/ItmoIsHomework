2a51e7ef62eb76d18545ec24bf86b2cee9eb8c68
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-02-17 19:50:24 +0300

    Add project files.

---
+++ AppliedMath.Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppliedMath.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
