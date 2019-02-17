d1932394e0bccca9d28e085be7da4c837166649d
Fredi Kats <fredikats@outlook.com>
2019-02-18 01:48:03 +0300

    Add StatisticAggregation (Lab 1, first task)

---
+++ AppliedMath.Lab1/DataReader.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace AppliedMath.Lab1
+{
+    public static class DataReader
+    {
+        public static string[] ReadInput(string filePath)
+        {
+            return File
+                .ReadAllText(filePath)
+                .Replace('\n', ' ')
+                .Replace("  ", " ")
+                .Split(" ");
+        }
+    }
+}
--- AppliedMath.Lab1/Program.cs
+++ AppliedMath.Lab1/Program.cs
@@ -7,6 +7,8 @@ namespace AppliedMath.Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string[] data = DataReader.ReadInput("InputData.txt");
+            Console.WriteLine(data.Length);
         }
     }
 }
