6216f7faa8e90f177fd57c8c5182fa25904c41c2
Fredi Kats <fredikats@outlook.com>
2019-03-07 20:42:45 +0300

    [Lab2] Markov Chain bruteforce implementation

---
+++ AppliedMath.Lab2/MathHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace AppliedMath.Lab2
+{
+    public class MathHelper
+    {
+        public static double LengthToVector(Vector<double> first, Vector<double> other)
+        {
+            Vector<double> delta = first.Subtract(other);
+            return delta.Select(v => Math.Pow(v, 2)).Sum();
+        }
+    }
+}
---
+++ AppliedMath.Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace AppliedMath.Lab2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Matrix<double> m = Matrix.Build.DenseOfRowArrays(MatrixReader.ReadMatrix()).Transpose();
+            Vector<double> current = Vector.Build.Dense(new double[] { 1, 0, 0, 0, 0, 0, 0, 0 });
+
+            Vector<double> result = MarkExecutor.RunBrutForce(m, current);
+        }
+    }
+}
