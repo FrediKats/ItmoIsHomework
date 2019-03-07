3be7775251ec0643697359f0a9d68140eb6ba43d
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-03-07 20:42:45 +0300

    [Lab2] Markov Chain bruteforce implementation

---
+++ AppliedMath.Lab2/MarkovChainExecutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace AppliedMath.Lab2
+{
+    public class MarkovChainExecutor
+    {
+        public static Vector<double> RunBrutForce(Matrix<double> m, Vector<double> current)
+        {
+            Vector<double> prev;
+
+            do
+            {
+                prev = current;
+                current = m.Multiply(current);
+                Console.WriteLine(current.ToString());
+                Console.WriteLine(MathHelper.LengthToVector(prev, current));
+            } while (MathHelper.LengthToVector(prev, current) > 1e-15);
+
+            return current;
+        }
+    }
+}
