bbd55d809728c967955fa5419fa59cf2315b4243
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-03-15 12:29:01 +0300

    Add UI project with diagrams

---
+++ ServeYourself.Client/UserControllers/VisitableStatisticController.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using OxyPlot;
+using ServeYourself.Client.Tools;
+
+namespace ServeYourself.Client.UserControllers
+{
+    public partial class VisitableStatisticController : UserControl
+    {
+        public IList<DataPoint> Points { get; private set; }
+
+        public VisitableStatisticController()
+        {
+            Points = new List<DataPoint>();
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public void GeneratePoint()
+        {
+            Points.Add(new DataPoint(Points.Count, RandomInstance.Random.Next(20)));
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GeneratePoint();
+            MainChart.InvalidatePlot(true);
+        }
+    }
+}
