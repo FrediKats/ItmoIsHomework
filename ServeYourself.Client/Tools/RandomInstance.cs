0562519b91b8ab2eed1d9f031e3636667ad3d532
Fredi Kats <fredikats@outlook.com>
2019-03-15 12:29:01 +0300

    Add UI project with diagrams

---
+++ ServeYourself.Client/Tools/RandomInstance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ServeYourself.Client.Tools
+{
+    public static class RandomInstance
+    {
+        public static readonly Random Random;
+
+        static RandomInstance()
+        {
+            Random = new Random();
+        }
+    }
+}
