fdeb386031de02c7235f6de4c88c714e988b0cb0
Fredi Kats <fredikats@outlook.com>
2019-03-30 17:46:40 +0300

    [Lab3] Try to add Promises

--- lab3/index.js
+++ lab3/index.js
@@ -1,9 +1,8 @@
 var quoteLink = "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote";
 var imgSourceLink = "https://source.unsplash.com/collection/1127163/500x500";
 
-var canvas = document.createElement('canvas');
-
 function createHtmlElements() {
+    var canvas = document.createElement('canvas');
     canvas.height = 500;
     canvas.width = 500;
     document.body.appendChild(canvas);
@@ -11,33 +10,41 @@ function createHtmlElements() {
     var btn = document.createElement('button');
     btn.innerText = "Save";
     document.body.appendChild(btn);
+
+    return new Promise(function (resolve, reject) {
+        resolve(canvas);
+    });
+
 }
+
+var canvasCtx;
 
 function parseQuote(response) {
     text = response.quoteText;
-    var ctx = canvas.getContext("2d");
-    ctx.font = "20px serif";
-    ctx.fillText(text, 100, 100);
-    
-    console.log(text);
+    canvasCtx.font = "20px serif";
+    canvasCtx.fillText(text, 100, 100);
 }
 
-function loadQuota() {
+function loadQuota(canvas) {
     $.ajax({
         url: quoteLink,
         dataType: 'jsonp'
     });
+    canvasCtx = canvas.getContext("2d");
 }
 
-function loadImage() {
+function loadImage(canvas) {
     var img = new Image();
     img.src = imgSourceLink;
-    img.onload = function () {
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-        loadQuota();
-    }
+    return new Promise(function (resolve, reject) {
+        img.onload = function () {
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+            resolve(canvas)
+        }
+    })
 }
 
-createHtmlElements();
-loadImage();
+createHtmlElements()
+    .then(canvas => loadImage(canvas))
+    .then(canvas => loadQuota(canvas));
