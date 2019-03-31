d50a225ec337d4ecfc853d3dd01a83350d5975a6
Fredi Kats <fredikats@outlook.com>
2019-03-31 09:55:02 +0300

    [Lab5] Adding markdown

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <textarea :value="input" @input="update"></textarea>
+    <div v-html="compiledMarkdown"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as marked from "marked";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+
+  data: function() {
+    return {
+      input: "__testing text__"
+    };
+  },
+
+  methods: {
+    update: function(element) {
+      this.input = element.target.value;
+    }
+  },
+
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.input);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  margin: 0;
+  height: 100%;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  color: #333;
+}
+
+textarea, #app div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
 }
 </style>
