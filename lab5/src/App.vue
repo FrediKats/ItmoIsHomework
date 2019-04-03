b73d8a203a1f8d0375d310b5592a8c5101f59ce4
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-04-03 10:41:53 +0300

    [Lab5] Add sending request, CORS bug

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
+    <div class="input-container">
+      <input type="text" :value="title">
+      <button v-on:click="save(title,input)">save</button>
+    </div>
+
     <textarea :value="input" @input="update"></textarea>
-    <div v-html="compiledMarkdown"></div>
+    <div class="output-block" v-html="compiledMarkdown"></div>
   </div>
 </template>
 
@@ -13,13 +18,29 @@ export default {
 
   data: function() {
     return {
-      input: "__testing text__"
+      input: "__testing text__",
+      title: "Testing header"
     };
   },
 
   methods: {
     update: function(element) {
       this.input = element.target.value;
+    },
+    save: function(title, content) {
+      const body = {
+        title,
+        content
+      };
+      fetch("http://localhost:3000/save", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      });
     }
   },
 
@@ -32,14 +53,21 @@ export default {
 </script>
 
 <style>
-html, body, #app {
+.input-container {
+  width: 100%;
+}
+
+html,
+body,
+#app {
   margin: 0;
   height: 100%;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Helvetica Neue", Arial, sans-serif;
   color: #333;
 }
 
-textarea, #app div {
+textarea,
+#app .output-block {
   display: inline-block;
   width: 49%;
   height: 100%;
@@ -55,7 +83,7 @@ textarea {
   outline: none;
   background-color: #f6f6f6;
   font-size: 14px;
-  font-family: 'Monaco', courier, monospace;
+  font-family: "Monaco", courier, monospace;
   padding: 20px;
 }
 
