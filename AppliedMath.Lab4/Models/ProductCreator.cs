d8b21a59650a14100a4946dccb0e05690d46d705
Fredi Kats <fredikats@outlook.com>
2019-04-04 00:28:52 +0300

    [Lab4] Task-2 simulation

---
+++ AppliedMath.Lab4/Models/ProductCreator.cs
@@ -0,0 +1,24 @@
+﻿namespace AppliedMath.Lab4.Models
+{
+    public class ProductCreator : ITransition
+    {
+        public string GetTransitionName()
+        {
+            return "Product creating";
+        }
+
+        public bool Invoke(SystemState state)
+        {
+            if (state.K >= 1 && state.M >= 2 && state.N >= 3 && state.S < 2)
+            {
+                state.K -= 1;
+                state.M -= 2;
+                state.N -= 3;
+                state.S += 1;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ AppliedMath.Lab4/Models/ResourceGenerator.cs
@@ -0,0 +1,40 @@
+﻿namespace AppliedMath.Lab4.Models
+{
+    public class ResourceGenerator : ITransition
+    {
+        private int _state = 0;
+
+        public string GetTransitionName()
+        {
+            switch (_state)
+            {
+                case 0:
+                    return "Added K";
+                case 1:
+                    return "Added M";
+                case 2:
+                    return "Added N";
+            }
+            return "ResourceCreate";
+        }
+
+        public bool Invoke(SystemState state)
+        {
+            switch (_state)
+            {
+                case 0:
+                    state.K++;
+                    break;
+                case 1:
+                    state.M++;
+                    break;
+                case 2:
+                    state.N++;
+                    break;
+            }
+
+            _state = (_state + 1) % 3;
+            return true;
+        }
+    }
+}
