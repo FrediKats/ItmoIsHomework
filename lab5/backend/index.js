df80a9cc3b57363cec95863f54ccaf8e49c54b4b
Fredi Kats <fredikats@outlook.com>
2019-04-03 10:41:53 +0300

    [Lab5] Add sending request, CORS bug

--- lab5/backend/index.js
+++ lab5/backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
@@ -11,16 +12,53 @@ mongoose
         useNewUrlParser: true
     })
     .then(
-        result => {
+        () => {
+            // eslint-disable-next-line no-console
             console.info("Connected")
         },
         error => {
+            // eslint-disable-next-line no-console
             console.info("MongoDB connection error" + error)
         });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const markdownFileModel = mongoose.model('MarkdownFileModel', {
+    title: String,
+    content: String
+});
 
-})
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Access-Control-Allow-Origin');
 
+    next();
+    
+});
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+app.post('/save', (request, response) => {
+    markdownFileModel.findOneAndUpdate({
+        name: request.body.title
+    }, {
+        text: request.body.text
+    }, {
+        upsert: true
+    }, function (err) {
+        if (err)
+            // eslint-disable-next-line no-console
+            console.log(err);
+        else
+            return response.sendStatus(200);
+    });
+});
+
+app.get('*', function (request, response) {
+    response.status(404).sendStatus('404');
+});
+
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
