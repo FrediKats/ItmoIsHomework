600fb54a772e18527ff08828070339b5aea6de68
Fredi Kats <fredikats@outlook.com>
2019-04-04 21:00:51 +0300

    Add diagrams for visualization queue size

--- AppliedMath.LoadBalancer/MainWindow.xaml.cs
+++ AppliedMath.LoadBalancer/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using AppliedMath.LoadBalancer.Models;
+using OxyPlot;
+using OxyPlot.Wpf;
 
 namespace AppliedMath.LoadBalancer
 {
@@ -26,11 +32,39 @@ namespace AppliedMath.LoadBalancer
                 }
             });
             _queueGenerator = new QueueGenerator(logger);
+
+            _dataPoints = Enumerable.Range(0, Models.LoadBalancer.Size).Select(i => new List<DataPoint>()).ToArray();
+            foreach (List<DataPoint> dataPoints in _dataPoints)
+            {
+                MainChart.Series.Add(new LineSeries() {ItemsSource = dataPoints});
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            _queueGenerator.Start();   
+            _queueGenerator.Start();
+            Task.Run(PlotUpdater);
+        }
+
+        private readonly List<DataPoint>[] _dataPoints;
+        private void PlotUpdater()
+        {
+            while (true)
+            {
+                List<int> sizes = _queueGenerator.Balancer.GetSizes();
+                for (int i = 0; i < sizes.Count; i++)
+                {
+                    AddPoint(_dataPoints[i], sizes[i]);
+                }
+
+                Application.Current.Dispatcher.Invoke(() => MainChart.InvalidatePlot());
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void AddPoint(List<DataPoint> list, int value)
+        {
+            list.Add(new DataPoint(list.Count, value));
         }
     }
 }
---
+++ AppliedMath.LoadBalancer/Tools/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AppliedMath.LoadBalancer.Tools
+{
+    public static class Config
+    {
+        public static readonly Random Random = new Random();
+
+        public static int MaxTaskDelay = 3000;
+    }
+}
