e1319730c1473de97513cf29478358b16d21b649
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-04-04 20:03:36 +0300

    [Project] Init for load balancer

---
+++ AppliedMath.LoadBalancer/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using AppliedMath.LoadBalancer.Models;
+
+namespace AppliedMath.LoadBalancer
+{
+    public partial class MainWindow : Window
+    {
+        private readonly QueueGenerator _queueGenerator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var logger = new Logger((s) =>
+            {
+                LogList.Items.Add(s);
+                if (VisualTreeHelper.GetChildrenCount(LogList) > 0)
+                {
+                    var border = (Border)VisualTreeHelper.GetChild(LogList, 0);
+                    var scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                    scrollViewer.ScrollToBottom();
+                }
+            });
+            _queueGenerator = new QueueGenerator(logger);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _queueGenerator.Start();   
+        }
+    }
+}
