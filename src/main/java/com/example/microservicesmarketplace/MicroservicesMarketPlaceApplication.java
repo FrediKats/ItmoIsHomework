d8b17f173d59132363a64ffc8eb70eb3c0c5fa2a
Fredi Kats <fredikats@outlook.com>
2019-09-21 19:33:08 +0300

    init

---
+++ src/main/java/com/example/microservicesmarketplace/MicroservicesMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesMarketPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicesmarketplace/MicroservicesMarketPlaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.microservicesmarketplace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesMarketPlaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
