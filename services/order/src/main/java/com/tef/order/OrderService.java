4ab851377cdfd1e8da5eef6022ee84aa84fd8dc8
Fredi Kats <fredikats@outlook.com>
2019-10-11 22:54:33 +0300

    chore: order service migrate to gradle

--- services/order/src/main/java/com/tef/order/OrderService.java
+++ services/order/src/main/java/com/tef/order/OrderService.java
@@ -24,17 +24,17 @@ public class OrderController {
 
 	@GetMapping("api/orders/{order_id}")
 	public void getOrderById(@PathVariable Integer order_id) {
-		return orderService.getOrderById(order_id);
+//		return orderService.getOrderById(order_id);
 	}
 
 	@PutMapping("api/orders/{order_id}/item/{item_id}")
 	public void addItemToOrder(@PathVariable Integer order_id, @PathVariable Integer item_id) {
-		return orderService.addItemToOrder(order_id, item_id);
+//		return orderService.addItemToOrder(order_id, item_id);
 	}
 
 	@PostMapping("api/orders/{order_id}/status/{status}")
 	public void changeOrderStatus(@PathVariable Integer order_id, @PathVariable OrderStatus status) {
-		return orderService.changeOrderStatus(order_id, status);
+//		return orderService.changeOrderStatus(order_id, status);
 	}
 
 	@DeleteMapping("api/orders/{order_id}/remove/")
