0f59f5a10b9e52e13dd1ca6bbfd0e9f93e7714d9
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-10-12 09:57:06 +0300

    fix: apply IoC fix for order and payment services

--- services/payment/src/main/java/com/tef/payment/services/PaymentService.java
+++ services/payment/src/main/java/com/tef/payment/services/PaymentService.java
@@ -3,7 +3,9 @@ package com.tef.payment.services;
 import com.tef.payment.dtos.PaymentInfoDto;
 import com.tef.payment.repositories.OrderInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentService {
     @Autowired
     private OrderInfoRepository orderInfoRepository;
