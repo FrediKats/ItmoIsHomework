7bc598d8925a9c767b8a26e56dc0811b0f116ba4
Maria Shipkova <maryshipkova@gmail.com>
2019-10-12 10:24:09 +0300

    added more logging

--- services/order/src/main/java/com/tef/order/repositories/OrderItemRepository.java
+++ services/order/src/main/java/com/tef/order/repositories/OrderItemRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderItemRepository extends CrudRepository<OrderItem, Integer>  {
+public interface OrderItemRepository extends CrudRepository<OrderItem, OrderItemId>  {
 }
--- services/warehouse/src/main/java/com/tef/warehouse/services/WarehouseService.java
+++ services/warehouse/src/main/java/com/tef/warehouse/services/WarehouseService.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 public class WarehouseService {
     @Autowired
     private ItemRepository itemRepository;
-
+    private Logger logger = LoggerFactory.getLogger(WarehouseService.class);
     public ItemDto addProduct(@PathVariable Integer id, @PathVariable Integer amount) throws Exception {
         Optional<Item> item = itemRepository.findById(id);
         if (item.isEmpty())
@@ -32,6 +32,7 @@ public class WarehouseService {
     }
 
     public List<ItemDto> getItems() {
+
         logger.debug("Get items");
         return StreamSupport
                 .stream(itemRepository
