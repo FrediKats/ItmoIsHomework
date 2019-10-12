7575d223f6e2edd0aa90f773856b8b1563d19529
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-10-12 10:19:48 +0300

    chore: delete OrderStatusLog

--- services/payment/src/main/java/com/tef/payment/repositories/OrderStatusLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tef.payment.repositories;
-
-import com.tef.payment.models.OrderStatusLog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderStatusLogRepository  extends CrudRepository<OrderStatusLog, Integer> {
-}
--- services/payment/src/main/java/com/tef/payment/services/PaymentService.java
+++ services/payment/src/main/java/com/tef/payment/services/PaymentService.java
@@ -1,61 +1,40 @@
 package com.tef.payment.services;
 
-import com.tef.payment.dtos.OrderDto;
 import com.tef.payment.dtos.PaymentInfoDto;
 import com.tef.payment.dtos.UserDetailDto;
 import com.tef.payment.models.OrderInfo;
-import com.tef.payment.models.OrderStatusLog;
 import com.tef.payment.repositories.OrderInfoRepository;
-import com.tef.payment.repositories.OrderStatusLogRepository;
 import com.tef.payment.types.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class PaymentService {
-  @Autowired
-  private OrderInfoRepository orderInfoRepository;
-  private OrderStatusLogRepository orderStatusLogRepository;
+    @Autowired
+    private OrderInfoRepository orderInfoRepository;
 
-  //TODO: изменить с void
-  void performPayment(Integer orderId, UserDetailDto userDetailDto) throws Exception {
-    Optional<OrderInfo> orderInfo = orderInfoRepository.findById(orderId);
-    if (!orderInfo.isPresent())
-      throw new Exception("order not found: " + orderId);
+    //TODO: изменить с void
+    public void performPayment(Integer orderId, UserDetailDto userDetailDto) throws Exception {
+        Optional<OrderInfo> orderInfo = orderInfoRepository.findById(orderId);
+        if (!orderInfo.isPresent())
+            throw new Exception("order not found: " + orderId);
 
-    OrderInfo instance = orderInfo.get();
-    instance.setUsername(userDetailDto.getUserName());
+        OrderInfo instance = orderInfo.get();
 
-    Optional<OrderStatusLog> orderStatusLog = orderStatusLogRepository.findById(orderId);
-    if (!orderStatusLog.isPresent())
-      throw new Exception("order not found: " + orderId);
+        orderInfoRepository.save(instance);
+    }
 
-    OrderStatusLog instanceLog = orderStatusLog.get();
-    instanceLog.setNewStatus(OrderStatus.Payed);
+    //TODO: разобраться с OrderDto
+    //TODO: изменить с void
+    public void addPaymentInfo(PaymentInfoDto paymentInfoDto) throws Exception {
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.setUsername(paymentInfoDto.getUserName());
+        orderInfo.setOrderId(paymentInfoDto.getOrderId());
+        orderInfoRepository.save(orderInfo);
+    }
 
-    orderInfoRepository.save(instance);
-    orderStatusLogRepository.save(instanceLog);
+    //TODO: изменить с void
+    public void cancelPayment(Integer orderId) throws Exception {
 
-  }
-
-  //TODO: разобраться с OrderDto
-  //TODO: изменить с void
-  void addPaymentInfo(PaymentInfoDto paymentInfoDto) throws Exception {
-    OrderInfo orderInfo = new OrderInfo();
-    orderInfo.setUsername(paymentInfoDto.getUserName());
-    orderInfo.setOrderId(paymentInfoDto.getOrderId());
-    orderInfoRepository.save(orderInfo);
-  }
-
-  //TODO: изменить с void
-  void cancelPayment(Integer orderId) throws Exception {
-    Optional<OrderStatusLog> orderStatusLog = orderStatusLogRepository.findById(orderId);
-
-    if (!orderStatusLog.isPresent())
-      throw new Exception("order not found: " + orderId);
-
-    OrderStatusLog instanceLog = orderStatusLog.get();
-    instanceLog.setNewStatus(OrderStatus.Canceled);
-    orderStatusLogRepository.save(instanceLog);
-  }
+    }
 }
