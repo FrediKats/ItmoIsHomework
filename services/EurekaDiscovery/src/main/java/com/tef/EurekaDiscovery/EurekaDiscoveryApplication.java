ba3008e914d6a774aa02fb5da305f54b4e6d513b
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-12-08 18:16:38 +0300

    chore: add project for eurica

---
+++ services/EurekaDiscovery/src/main/java/com/tef/EurekaDiscovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.tef.EurekaDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
