23e5cdeccc580c356334d3c2d8c3ad579b7bc2c2
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-12-08 14:32:22 +0300

    fix: Cannot convert from [[B] ...]

--- services/order/src/main/java/com/tef/order/services/RabbitMqListener.java
+++ services/order/src/main/java/com/tef/order/services/RabbitMqListener.java
@@ -4,21 +4,18 @@ import com.tef.order.dtos.OrderStatusUpdateMessage;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.util.logging.Logger;
 
 @EnableRabbit
 @Component
 public class RabbitMqListener {
-    Logger logger = Logger.getLogger(RabbitMqListener.class.getName());
-
     @Autowired
     private OrderService orderService;
 
     @RabbitListener(queues = "order-status-update")
-    public void OrderStatusUpdateProcess(OrderStatusUpdateMessage msg) throws Exception {
-        logger.info("Received from queue 1: " + msg);
+    public void OrderStatusUpdateProcess(@Payload OrderStatusUpdateMessage msg) throws Exception {
+        System.out.println(msg.getNewStatus() + ": " + msg.getOrderId());
         orderService.changeOrderStatus(msg.getOrderId(), msg.getNewStatus());
     }
 }
