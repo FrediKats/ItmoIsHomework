21ca6eb51bc34702686a7b915b63da7ff504335e
Fredi Kats <fredikats@outlook.com>
2019-12-08 14:00:04 +0300

    feat: using rabbit queue

---
+++ services/order/src/main/java/com/tef/order/services/RabbitConfiguration.java
@@ -0,0 +1,41 @@
+package com.tef.order.services;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.logging.Logger;
+
+@Configuration
+public class RabbitConfiguration {
+    Logger logger = Logger.getLogger(RabbitConfiguration.class.getName());
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory =
+                new CachingConnectionFactory("localhost");
+        return connectionFactory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+    @Bean
+    public Queue OrderStatusUpdateQueue() {
+        return new Queue("order-status-update");
+    }
+}
--- services/order/src/main/java/com/tef/order/services/RabbitReciver.java
+++
@@ -1,4 +0,0 @@
-package com.tef.order.services;
-
-public class RabbitReciver {
-}
