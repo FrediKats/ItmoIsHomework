14217b673b345a89871c1f914a30030633dcc70b
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-12-21 02:44:09 +0300

    fix: add check for order existing in payment service

--- services/payment/src/main/java/com/tef/payment/services/PaymentService.java
+++ services/payment/src/main/java/com/tef/payment/services/PaymentService.java
@@ -1,21 +1,17 @@
 package com.tef.payment.services;
 
-import com.tef.payment.dtos.OrderStatusUpdateMessage;
-import com.tef.payment.dtos.PaymentInfoDto;
-import com.tef.payment.dtos.UserDetailDto;
-import com.tef.payment.models.OrderInfo;
+import com.tef.payment.dtos.*;
 import com.tef.payment.repositories.OrderInfoRepository;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import com.tef.payment.types.CardAuthorizationInfo;
 import com.tef.payment.types.OrderStatus;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Optional;
 
 @EnableBinding(Source.class)
 @Service
@@ -23,20 +19,14 @@ public class PaymentService {
     private final String orderServiceUrl = "http://localhost:8182/api/orders/";
 
     @Autowired
-    private OrderInfoRepository orderInfoRepository;
-
-    @Autowired
     AmqpTemplate template;
 
-    @Autowired
-    private Source mysource;
-
     public OrderStatus performPayment(Integer orderId, UserDetailDto userDetailDto) throws Exception {
-        //Optional<OrderInfo> orderInfo = orderInfoRepository.findById(orderId);
-        //if (!orderInfo.isPresent())
-        //    throw new Exception("order not found: " + orderId);
+        var order = getOrderFromService(orderId);
+        if (order == null)
+            throw new Exception("order not found: " + orderId);
 
-        OrderStatus instance = OrderStatus.Payed;
+        OrderStatus instance;
         if (userDetailDto.getCardAuthorizationInfo() == CardAuthorizationInfo.AUTHORIZED)
             instance = (OrderStatus.Payed);
         else
@@ -50,39 +40,25 @@ public class PaymentService {
         return instance;
     }
 
-    //TODO: разобраться с OrderDto
-    public OrderStatus addPaymentInfo(PaymentInfoDto paymentInfoDto) throws Exception {
-        OrderInfo orderInfo = new OrderInfo();
-        orderInfo.setUsername(paymentInfoDto.getUserName());
-        orderInfo.setOrderId(paymentInfoDto.getOrderId());
-        orderInfo.setOrderStatus(OrderStatus.Collecting);
-
-        OrderStatusUpdateMessage orderStatusUpdateMessage = new OrderStatusUpdateMessage();
-        orderStatusUpdateMessage.setNewStatus(orderInfo.getOrderStatus());
-        orderStatusUpdateMessage.setOrderId(orderInfo.getOrderId());
-        updateStateRemote(orderStatusUpdateMessage);
-
-        orderInfoRepository.save(orderInfo);
-        return orderInfo.getOrderStatus();
-    }
-
     public OrderStatus cancelPayment(Integer orderId) throws Exception {
-        //Optional<OrderInfo> orderInfo = orderInfoRepository.findById(orderId);
-        //if (!orderInfo.isPresent())
-         //   throw new Exception("order not found: " + orderId);
+        var order = getOrderFromService(orderId);
+        if (order == null)
+            throw new Exception("order not found: " + orderId);
 
-        //OrderInfo instance = orderInfo.get();
-        //instance.setOrderStatus(OrderStatus.Canceled);
         OrderStatusUpdateMessage orderStatusUpdateMessage = new OrderStatusUpdateMessage();
         orderStatusUpdateMessage.setNewStatus(OrderStatus.Canceled);
         orderStatusUpdateMessage.setOrderId(orderId);
         updateStateRemote(orderStatusUpdateMessage);
-        //orderInfoRepository.save(instance);
-        //TODO: remove order from order service
         return OrderStatus.Canceled;
     }
 
     private void updateStateRemote(OrderStatusUpdateMessage orderStatusUpdateInfo) {
         template.convertAndSend("order-status-update", orderStatusUpdateInfo);
+    }
+
+    private OrderDto getOrderFromService(Integer id) {
+        String getItemUrl = orderServiceUrl + "/orders/" + id.toString();
+        OrderDto order =  new RestTemplate().getForObject(getItemUrl, OrderDto.class);
+        return order;
     }
 }
