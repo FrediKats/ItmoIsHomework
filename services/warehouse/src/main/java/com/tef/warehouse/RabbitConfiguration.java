97acc43adaa5f12afcffdb1e0bb2329f88ca15fe
Maksim Barkalov <maksimbark@yandex-team.ru>
2019-12-21 03:15:59 +0300

    fix warehouse conf

--- services/warehouse/src/main/java/com/tef/warehouse/RabbitConfiguration.java
+++ services/warehouse/src/main/java/com/tef/warehouse/RabbitConfiguration.java
@@ -15,7 +15,7 @@ public class RabbitConfiguration {
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
+                new CachingConnectionFactory("rabbitmq");
         return connectionFactory;
     }
 
