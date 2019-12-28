8fbe245a0e75cf5cf1aece894d924f9b20699488
Maksim Barkalov <maksimbark@yandex-team.ru>
2019-12-28 11:33:39 +0300

    fixup! починил gateway

--- services/zuul-proxy/src/main/java/com/tef/zuulproxy/ZuulProxyApplication.java
+++ services/zuul-proxy/src/main/java/com/tef/zuulproxy/ZuulProxyApplication.java
@@ -2,8 +2,13 @@ package com.tef.zuulproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ZuulProxyApplication {
 
 	public static void main(String[] args) {
