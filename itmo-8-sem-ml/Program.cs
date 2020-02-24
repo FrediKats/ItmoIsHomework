9f2dbaff60adfd97c0dba9ef729f3d69e3cabd36
Fredi Kats <fredikats@outlook.com>
2020-02-24 18:55:30 +0300

    add: lab c

--- itmo-8-sem-ml/Program.cs
+++ itmo-8-sem-ml/Program.cs
@@ -4,19 +4,26 @@ using System.Linq;
 
 namespace itmo_8_sem_ml
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            
+            var c = new TaskC();
         }
+
+        public static Int32[] ReadArray()
+        {
+            return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        }
+
+        public static T2 Return<T1, T2>(this T1 input, Func<T1, T2> result) => result(input);
     }
 
     public class TaskA
     {
         public static void Run()
         {
-            var input = Console.ReadLine().Split(' ').Select(int.Parse);
+            var input = Program.ReadArray();
             Console.ReadLine()
                 .Split(' ')
                 .Select((s, i) => (Value: int.Parse(s), Index: i + 1))
@@ -111,4 +118,131 @@ namespace itmo_8_sem_ml
             private double ValueOrZero(double value) => double.IsNaN(value) ? 0 : value;
         }
     }
+
+    public class TaskC
+    {
+        public TaskC()
+        {
+            Int32[] input = Program.ReadArray();
+            List<(Int32[], int)> dataSet = new List<(Int32[], int)>(input[0]);
+            for (int i = 0; i < input[0]; i++)
+            {
+                var c = Program.ReadArray();
+
+                dataSet.Add((c.Take(input[1]).ToArray(), c.Last()));
+            }
+            Int32[] target = Program.ReadArray();
+
+            var context = new Context(
+                dataSet,
+                target,
+                Console.ReadLine(),
+                Console.ReadLine(),
+                Console.ReadLine(),
+                Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine(context.Execute());
+        }
+
+        
+
+        public class Context
+        {
+            public Context(List<(Int32[], int)> dataSet, Int32[] targetPoint, String distType, String coreType, String windowType, Int32 windowArgument)
+            {
+                DataSet = dataSet;
+                TargetPoint = targetPoint;
+                DistType = distType;
+                CoreType = coreType;
+                WindowType = windowType;
+                WindowArgument = windowArgument;
+            }
+
+            public Int32[] TargetPoint { get; set; }
+            public String DistType { get; set; }
+            public String CoreType { get; set; }
+            public String WindowType { get; set; }
+            public Int32 WindowArgument { get; set; }
+            public List<(Int32[], int)> DataSet { get; set; }
+
+            public double Execute()
+            {
+                var distFunc = GetDistFunc(DistType);
+                Func<double, double> funcK = GetFuncK(CoreType);
+
+                IEnumerable<(Double, Int32)> dists = DataSet
+                    .Select(p => (distFunc(p.Item1, TargetPoint), p.Item2));
+
+                double window = GetWindow(dists);
+
+                var cores = dists
+                    .Select(d => (d.Item1 == 0 ? funcK(d.Item1) : window == 0.0 ? 0 : funcK(d.Item1 / window),
+                        d.Item2));
+
+                if (cores.Sum(c => c.Item1) == 0)
+                    return cores.Average(c => c.Item2);
+
+                return cores
+                    .Select(c => c.Item1 * (c.Item2))
+                    .Sum()
+                    .Return(s => s / cores.Sum(c => c.Item1));
+            }
+
+            public Func<double, double> GetFuncK(string funcName)
+            {
+                switch (funcName)
+                {
+                    case "uniform":
+                        return u => 0.5.Return(v => Math.Abs(u) >= 1.0 ? 0 : v);
+                    case "triangular":
+                        return u => 1 - Math.Abs(u).Return(v => Math.Abs(u) > 1.0 ? 0 : v);
+                    case "epanechnikov":
+                        return u => 0.75 * (1 - Math.Pow(u, 2)).Return(v => Math.Abs(u) > 1.0 ? 0 : v);
+                    case "quartic":
+                        return u => (15.0 / 16) * Math.Pow(1 - Math.Pow(u, 2), 2).Return(v => Math.Abs(u) > 1.0 ? 0 : v);
+                    case "triweight":
+                        return u => (35.0 / 32) * Math.Pow(1 - Math.Pow(u, 2), 3).Return(v => Math.Abs(u) > 1.0 ? 0 : v);
+                    case "tricube":
+                        return u => (70.0 / 81) * Math.Pow(1 - Math.Pow(Math.Abs(u), 3), 3).Return(v => Math.Abs(u) > 1.0 ? 0 : v);
+                    case "gaussian":
+                        return u => (1.0 / Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -1.0 / 2 * Math.Pow(u, 2));
+                    case "cosine":
+                        return u => Math.PI / 4 * Math.Cos(Math.PI / 2 * u).Return(v => Math.Abs(u) > 1.0 ? 0 : v);
+                    case "logistic":
+                        return u => 1.0 / (Math.Pow(Math.E, u) + 2 + Math.Pow(Math.E, -u));
+                    case "sigmoid":
+                        return u => 2.0 / Math.PI / (Math.Pow(Math.E, u) + Math.Pow(Math.E, -u));
+                }
+
+                throw new NotSupportedException();
+            }
+
+            public Func<int[], int[], double> GetDistFunc(string funcName)
+            {
+                switch (funcName)
+                {
+                    case "manhattan":
+                        return (a, b) => a.Zip(b, (ae, be) => (double) ae - be).Sum(Math.Abs);
+                    case "euclidean":
+                        return (a, b) => a
+                            .Zip(b, (ae, be) => (double) ae - be)
+                            .Select(s => Math.Pow(s, 2))
+                            .Sum()
+                            .Return(Math.Sqrt);
+                    case "chebyshev":
+                        return (a, b) => a.Zip(b, (ae, be) => (double) ae - be).Max(Math.Abs);
+                }
+
+                throw new NotSupportedException();
+            }
+
+            public double GetWindow(IEnumerable<(Double, Int32)> elements)
+            {
+                return WindowType == "fixed"
+                    ? WindowArgument
+                    : elements.OrderBy(e => e.Item1).ElementAt(WindowArgument).Item1;
+            }
+        }
+    }
+
 }
