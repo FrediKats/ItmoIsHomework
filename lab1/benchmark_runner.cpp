45ca1e7fb4795176fea4dc81f0d9f0137d04f2cc
Fredi Kats <fredikats@outlook.com>
2021-09-26 12:41:13 +0300

    refactor: move benchmark to class

---
+++ lab1/benchmark_runner.cpp
@@ -0,0 +1,20 @@
+﻿#include "benchmark_runner.h"
+
+lab1::benchmark_runner::benchmark_runner(const int run_count): run_count_(run_count)
+{
+}
+
+std::chrono::duration<double> lab1::benchmark_runner::benchmark_run(const std::function<void()>& functor) const
+{
+	std::chrono::duration<double> result = std::chrono::duration<double>::zero();
+	for (auto i = 0; i < run_count_; i++)
+	{
+		const auto start = std::chrono::high_resolution_clock::now();
+		functor();
+		const auto end = std::chrono::high_resolution_clock::now();
+
+		result += (end - start);
+	}
+
+	return result / run_count_;
+}
