1d63f7f21de1494b505667b92ed7006f93f1a603
Fredi Kats <fredikats@outlook.com>
2021-09-27 10:45:59 +0300

    feat: add color normalilzer

---
+++ omp2/color_normalizer.cpp
@@ -0,0 +1,42 @@
+﻿#include "color_normalizer.h"
+
+#include "functional_extension.h"
+
+std::vector<omp2::color> omp2::color_normalizer::modify(std::vector<color> input_colors, int delta, int coefficient)
+{
+	const auto red_selector = std::function<unsigned char(color)>([](const color c) { return c.red; });
+	const auto blue_selector = std::function<unsigned char(color)>([](const color c) { return c.blue; });
+	const auto green_selector = std::function<unsigned char(color)>([](const color c) { return c.green; });
+
+	auto result = std::vector<color>(input_colors.size());
+	for (size_t index = 0; index < input_colors.size(); index++)
+	{
+		const auto changed_color = color(
+			change_color(input_colors, index, delta, coefficient, red_selector),
+			change_color(input_colors, index, delta, coefficient, green_selector),
+			change_color(input_colors, index, delta, coefficient, blue_selector));
+		input_colors[index] = changed_color;
+	}
+
+	return input_colors;
+}
+
+unsigned char omp2::color_normalizer::change_color(
+	std::vector<color> input_colors,
+	size_t index,
+	int delta,
+	int coefficient,
+	const std::function<unsigned char(color)>& color_selector)
+{
+	auto minimal_value = functional_extension::map_fold(
+		input_colors,
+		color_selector,
+		std::function<color(std::vector<color>)>([](std::vector<color> colors) { return colors[0]; }));
+
+	auto max_value = functional_extension::map_fold(
+		input_colors,
+		color_selector,
+		std::function<color(std::vector<color>)>([](std::vector<color> colors) { return colors[colors.size() - 1]; }));
+
+	return (color_selector(input_colors[index]) + delta) * coefficient;
+}
--- omp2/omp2.cpp
+++ omp2/omp2.cpp
@@ -2,12 +2,18 @@
 #include <sstream>
 
 #include "color_reader.h"
+#include "color_normalizer.h"
 
 int main()
 {
-	omp2::color_reader reader = omp2::color_reader("img");
+	auto reader = omp2::color_reader("img");
+	auto color_normalizer = omp2::color_normalizer();
 
-	auto colors = reader.read();
+	std::vector<omp2::color> colors = reader.read();
+	//TODO: change type to double
+	auto modified_color = color_normalizer.modify(colors, 0, 1);
+
+
 
     std::cout << "Hello World!\n";
 }
