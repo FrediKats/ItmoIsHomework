abc884fe0b7447e9530623c2a97feaf6bef32db3
Fredi Kats <fredikats@outlook.com>
2021-09-27 10:45:59 +0300

    feat: add color normalilzer

---
+++ omp2/color_normalizer.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <functional>
+
+#include "color.h"
+#include <vector>
+
+namespace omp2
+{
+	class color_normalizer
+	{
+	public:
+		std::vector<color> modify(std::vector<color> input_colors, int delta, int coefficient);
+
+	private:
+		static unsigned char change_color(std::vector<color> input_colors, size_t index, int delta, int coefficient, const std::function<unsigned char(color)>
+		                                  & color_selector);
+	};
+
+	
+}
