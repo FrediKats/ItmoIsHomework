9cf3534d69c68b97f14cb55ca894af5cc3909ecf
Fredi Kats <fredikats@outlook.com>
2021-09-27 10:45:42 +0300

    feat: add fold function

---
+++ omp2/functional_extension.cpp
@@ -0,0 +1,1 @@
+﻿#include "functional_extension.h"
---
+++ omp2/functional_extension.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+namespace omp2
+{
+	class functional_extension
+	{
+	public:
+		template <class TInput, class TOutput>
+		static TOutput map_fold(std::vector<TInput> source,
+		                 const std::function<TOutput(TInput)>& selector,
+		                 const std::function<TInput(std::vector<TInput>)>& fold);
+	};
+
+	template <class TInput, class TOutput>
+	TOutput functional_extension::map_fold(std::vector<TInput> source,
+	                                       const std::function<TOutput(TInput)>& selector,
+	                                       const std::function<TInput(std::vector<TInput>)>& fold)
+	{
+		std::sort(
+			std::begin(source),
+			std::end(source),
+			[selector](const TInput a, const TInput b)
+			{
+				return selector(a) > selector(b);
+			});
+
+		return selector(fold(source));
+	}
+}
