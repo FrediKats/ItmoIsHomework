e8b0a6783d13e3f648848391a579f72f83f551ae
Fredi Kats <fredikats@outlook.com>
2021-10-02 22:53:11 +0300

    feat: add total min and max for three colors

--- omp2/color_normalizer.h
+++ omp2/color_normalizer.h
@@ -8,7 +8,7 @@ namespace omp2
 	class color_normalizer
 	{
 	public:
-		static constexpr int ignore_percent = 2560;
+		static constexpr int ignore_percent = 256;
 
 		static std::vector<color> modify(const std::vector<color>& input_colors);
 	};
