b503c1e4f110aec42813624e76744bc562983059
Fredi Kats <fredikats@outlook.com>
2021-10-03 13:10:25 +0300

    refactor: extract duplicate code from normalizers

--- omp2/color_morphism.cpp
+++ omp2/color_morphism.cpp
@@ -1,5 +1,7 @@
 ﻿#include "color_morphism.h"
 
+#include "color_histogram.h"
+
 namespace omp2
 {
 	color_morphism::color_morphism()
@@ -8,6 +10,20 @@ namespace omp2
 
 	color_morphism::color_morphism(const unsigned char min, const unsigned char max) : min_(min), max_(max)
 	{
+	}
+
+	color_morphism::color_morphism(const std::vector<color_histogram>& histograms)
+	{
+		unsigned char min = histograms[0].get_min_value();
+		unsigned char max = histograms[0].get_max_value();
+		for (auto& histogram : histograms)
+		{
+			min = std::min(min, histogram.get_min_value());
+			max = std::max(max, histogram.get_max_value());
+		}
+
+		min_ = min;
+		max_ = max;
 	}
 
 	unsigned char color_morphism::change_color(const unsigned char original_color_value) const
