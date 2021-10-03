d35e6630f3bb806dbb7f940ea49b49c15ccb0b16
Fredi Kats <fredikats@outlook.com>
2021-10-03 14:39:38 +0300

    feat: support passing arguments from console

--- omp2/pnm_image_descriptor.cpp
+++ omp2/pnm_image_descriptor.cpp
@@ -1,5 +1,7 @@
 ﻿#include "pnm_image_descriptor.h"
 
+#include <utility>
+
 omp2::pnm_image_descriptor::pnm_image_descriptor(
 	const int version,
 	std::vector<omp2::color> color,
@@ -12,4 +14,10 @@ omp2::pnm_image_descriptor::pnm_image_descriptor(
 	  height(height),
 	  max_value(max_value)
 {
+}
+
+omp2::pnm_image_descriptor omp2::pnm_image_descriptor::update_colors(std::vector<omp2::color> new_colors) const
+{
+	auto result = pnm_image_descriptor(version, std::move(new_colors), width, height, max_value);
+	return result;
 }
