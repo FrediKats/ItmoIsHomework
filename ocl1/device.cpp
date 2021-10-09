996a4513a4f8dce2bc7d886278bbccf6688242b4
Fredi Kats <fredikats@outlook.com>
2021-10-09 21:05:59 +0300

    feat: add model for device

---
+++ ocl1/device.cpp
@@ -0,0 +1,10 @@
+﻿#include "device.h"
+
+device::device()
+{
+}
+
+device::device(cl_device_id id, const std::string& name): id(id),
+                                                          name(name)
+{
+}
