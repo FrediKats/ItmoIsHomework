b8d8abac10432ea2c4e80c83f3967c7d4ccd2696
Fredi Kats <fredikats@outlook.com>
2021-10-10 01:47:50 +0300

    feat: add multiplication kernel argument

---
+++ ocl1/multiplication_kernel_argument.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "kernel_argument.h"
+
+class multiplication_kernel_argument : kernel_argument
+{
+public:
+	multiplication_kernel_argument(int n, int k, int m, float* a, float* b);
+
+	void write_arguments(execution_context execution_context_instance, cl_kernel kernel) override;
+
+private:
+	int n_;
+	int k_;
+	int m_;
+	float* matrix_a_;
+	float* matrix_b_;
+};
