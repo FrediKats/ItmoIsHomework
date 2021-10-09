163b8984b1e35c9e9caa7deefd9327eeee60a978
Fredi Kats <fredikats@outlook.com>
2021-10-09 18:40:25 +0300

    refactor: add device provider

--- ocl1/ocl1.cpp
+++ ocl1/ocl1.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <CL/opencl.h>
 #include <vector>
+#include "cl_device_provider.h"
 
 //NB: https://stackoverflow.com/a/28344440
 std::string get_file_string() {
@@ -12,94 +13,8 @@ std::string get_file_string() {
         (std::istreambuf_iterator<char>()));
 }
 
-std::vector<cl_platform_id> get_platforms()
-{
-	cl_uint num_platforms;
-	cl_platform_id* cl_selected_platform_id = nullptr;
-	// The only one way for detecting platform count
-	clGetPlatformIDs(0, nullptr, &num_platforms);
 
-	cl_selected_platform_id = static_cast<cl_platform_id*>(malloc(sizeof(cl_platform_id) * num_platforms));
-	const int err = clGetPlatformIDs(num_platforms, cl_selected_platform_id, nullptr);
-	if (err != CL_SUCCESS)
-	{
-		throw std::exception("Error: Failed to create a device group!");
-	}
 
-	auto result = std::vector<cl_platform_id>(num_platforms);
-    // NB: https://stackoverflow.com/a/25926679
-	const char* attributeNames[5] = { "Name", "Vendor", "Version", "Profile", "Extensions" };
-    const cl_platform_info attributeTypes[5] = {
-        CL_PLATFORM_NAME,
-                                                CL_PLATFORM_VENDOR,
-                                                CL_PLATFORM_VERSION,
-                                                CL_PLATFORM_PROFILE,
-                                                CL_PLATFORM_EXTENSIONS };
-
-    size_t infoSize;
-    for (int j = 0; j < 5; j++)
-    {
-        for (int i = 0; i < num_platforms; i++)
-        {
-            // get platform attribute value size
-            clGetPlatformInfo(cl_selected_platform_id[i], attributeTypes[j], 0, NULL, &infoSize);
-            auto info = (char*)malloc(infoSize);
-
-            // get platform attribute value
-            clGetPlatformInfo(cl_selected_platform_id[i], attributeTypes[j], infoSize, info, NULL);
-
-            //printf("  %d.%d %-11s: %s\n", i + 1, j + 1, attributeNames[j], info);
-			result[i] = cl_selected_platform_id[i];
-        }
-    }
-	
-
-	return result;
-}
-
-cl_device_id select_device(cl_platform_id platform_id, cl_device_type device_type)
-{
-    cl_uint device_count;
-    //TODO: select discrete video card
-    // use clGetDeviceIDS | clGetDeviceInfo
-    cl_int err = clGetDeviceIDs(platform_id, device_type, 0, nullptr, &device_count);
-
-    cl_device_id* device_ids = static_cast<cl_device_id*>(malloc(sizeof(cl_device_id) * device_count));
-    err = clGetDeviceIDs(platform_id, device_type, device_count, device_ids, &device_count);
-    for (int i = 0; i < device_count; i++)
-    {
-        size_t return_size;
-        clGetDeviceInfo(device_ids[i], CL_DEVICE_NAME, 0, nullptr, &return_size);
-
-        char* cBuffer = static_cast<char*>(malloc(sizeof(char) * return_size));
-        clGetDeviceInfo(device_ids[i], CL_DEVICE_NAME, return_size, cBuffer, &return_size);
-        std::string name = std::string(cBuffer);
-
-        std::cout << name;
-    }
-
-    if (err != CL_SUCCESS)
-    {
-        throw std::exception("Error: Failed to create a device group!");
-    }
-
-    return device_ids[0];
-}
-
-cl_device_id select_device(cl_platform_id platform_id)
-{
-    cl_uint device_count;
-    cl_device_id device_id;
-    //TODO: select discrete video card
-    // use clGetDeviceIDS | clGetDeviceInfo
-    cl_int err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 0, &device_id, &device_count);
-    if (err != CL_SUCCESS)
-    {
-        throw std::exception("Error: Failed to create a device group!");
-    }
-
-    return device_id;
-}
 
 int main()
 {
@@ -108,14 +23,9 @@ int main()
     cl_context context;
     cl_command_queue command_queue;
 
-
-    const auto cl_platform_ids = get_platforms();
-    //TODO: fix [0]
-    for (int i = 0; i < cl_platform_ids.size(); i++)
-    {
-        select_device(cl_platform_ids[i], CL_DEVICE_TYPE_GPU);
-    }
-	cl_device_id device_id = select_device(cl_platform_ids[0], CL_DEVICE_TYPE_GPU);
+    cl_device_provider device_provider = cl_device_provider();
+    //TODO: read index from args
+	cl_device_id device_id = device_provider.get_device_id(0);
 
 
     //create context
