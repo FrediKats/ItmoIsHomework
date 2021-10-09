48d3049c07d647f621ae0623f440429e83f868e4
Fredi Kats <fredikats@outlook.com>
2021-10-09 20:16:36 +0300

    feat: selecting device from full list

--- ocl1/cl_device_provider.cpp
+++ ocl1/cl_device_provider.cpp
@@ -6,89 +6,104 @@
 
 std::vector<cl_platform_id> get_platforms()
 {
-    cl_uint num_platforms;
-    cl_platform_id* cl_selected_platform_id = nullptr;
-    // The only one way for detecting platform count
-    clGetPlatformIDs(0, nullptr, &num_platforms);
-
-    cl_selected_platform_id = static_cast<cl_platform_id*>(malloc(sizeof(cl_platform_id) * num_platforms));
-    const int err = clGetPlatformIDs(num_platforms, cl_selected_platform_id, nullptr);
+	cl_uint num_platforms;
+    cl_int err = clGetPlatformIDs(0, nullptr, &num_platforms);
     if (err != CL_SUCCESS)
-    {
         throw std::exception("Error: Failed to create a device group!");
-    }
+
+	cl_platform_id* cl_selected_platform_id = static_cast<cl_platform_id*>(malloc(sizeof(cl_platform_id) * num_platforms));
+	err = clGetPlatformIDs(num_platforms, cl_selected_platform_id, nullptr);
+	if (err != CL_SUCCESS)
+		throw std::exception("Error: Failed to create a device group!");
+
+	// NB: https://stackoverflow.com/a/25926679
+	const char* attribute_names[5] = {"Name", "Vendor", "Version", "Profile", "Extensions"};
+	constexpr cl_platform_info attribute_types[5] = {
+		CL_PLATFORM_NAME,
+		CL_PLATFORM_VENDOR,
+		CL_PLATFORM_VERSION,
+		CL_PLATFORM_PROFILE,
+		CL_PLATFORM_EXTENSIONS
+	};
+
+	//TODO: remove output
+	size_t infoSize;
 
     auto result = std::vector<cl_platform_id>(num_platforms);
-    // NB: https://stackoverflow.com/a/25926679
-    const char* attributeNames[5] = { "Name", "Vendor", "Version", "Profile", "Extensions" };
-    const cl_platform_info attributeTypes[5] = {
-        CL_PLATFORM_NAME,
-                                                CL_PLATFORM_VENDOR,
-                                                CL_PLATFORM_VERSION,
-                                                CL_PLATFORM_PROFILE,
-                                                CL_PLATFORM_EXTENSIONS };
-
-    size_t infoSize;
     for (int i = 0; i < num_platforms; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            // get platform attribute value size
-            clGetPlatformInfo(cl_selected_platform_id[i], attributeTypes[j], 0, NULL, &infoSize);
-            auto info = (char*)malloc(infoSize);
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			// get platform attribute value size
+			clGetPlatformInfo(cl_selected_platform_id[i], attribute_types[j], 0, nullptr, &infoSize);
+			const auto info = static_cast<char*>(malloc(infoSize));
 
-            // get platform attribute value
-            clGetPlatformInfo(cl_selected_platform_id[i], attributeTypes[j], infoSize, info, NULL);
+			// get platform attribute value
+			clGetPlatformInfo(cl_selected_platform_id[i], attribute_types[j], infoSize, info, nullptr);
 
-            printf("  %d.%d %-11s: %s\n", i + 1, j + 1, attributeNames[j], info);
-            result[i] = cl_selected_platform_id[i];
-        }
-    }
+			printf("  %d.%d %-11s: %s\n", i + 1, j + 1, attribute_names[j], info);
+			result[i] = cl_selected_platform_id[i];
+		}
+	}
 
-
-    return result;
+	return result;
 }
 
-cl_device_id select_device(cl_platform_id platform_id, cl_device_type device_type)
+std::vector<cl_device_id> get_devices(cl_platform_id platform_id, cl_device_type device_type)
 {
     cl_uint device_count;
-    //TODO: select discrete video card
-    // use clGetDeviceIDS | clGetDeviceInfo
     cl_int err = clGetDeviceIDs(platform_id, device_type, 0, nullptr, &device_count);
+    if (err == CL_DEVICE_NOT_FOUND)
+        return std::vector<cl_device_id>();
 
-    cl_device_id* device_ids = static_cast<cl_device_id*>(malloc(sizeof(cl_device_id) * device_count));
+    if (err != CL_SUCCESS)
+        throw std::exception("Error: Failed to get devices ids for platform ");
+    auto device_ids = static_cast<cl_device_id*>(malloc(sizeof(cl_device_id) * device_count));
     err = clGetDeviceIDs(platform_id, device_type, device_count, device_ids, &device_count);
-    for (int i = 0; i < device_count; i++)
+    if (err != CL_SUCCESS)
+        throw std::exception("Error: Failed to get devices ids for platform ");
+
+    std::vector<cl_device_id> result = std::vector<cl_device_id>(device_count);
+	for (cl_uint i = 0; i < device_count; i++)
     {
         size_t return_size;
-        clGetDeviceInfo(device_ids[i], CL_DEVICE_NAME, 0, nullptr, &return_size);
+        err = clGetDeviceInfo(device_ids[i], CL_DEVICE_NAME, 0, nullptr, &return_size);
+        if (err != CL_SUCCESS)
+            throw std::exception("Error: Failed to get device info");
 
         char* cBuffer = static_cast<char*>(malloc(sizeof(char) * return_size));
-        clGetDeviceInfo(device_ids[i], CL_DEVICE_NAME, return_size, cBuffer, &return_size);
+        err = clGetDeviceInfo(device_ids[i], CL_DEVICE_NAME, return_size, cBuffer, &return_size);
+        if (err != CL_SUCCESS)
+            throw std::exception("Error: Failed to get device info");
+
         std::string name = std::string(cBuffer);
 
         std::cout << name;
+
+        result[i] = device_ids[i];
     }
 
-    if (err != CL_SUCCESS)
-    {
-        throw std::exception("Error: Failed to create a device group!");
-    }
-
-    return device_ids[0];
+    return result;
 }
 
 cl_device_id cl_device_provider::get_device_id(int requested_index)
 {
     //TODO: detect discrete and integrated cards
 
-
+    std::vector<cl_device_id> devices = std::vector<cl_device_id>();
     const auto cl_platform_ids = get_platforms();
-    //TODO: fix [0]
+
     for (int i = 0; i < cl_platform_ids.size(); i++)
     {
-        select_device(cl_platform_ids[i], CL_DEVICE_TYPE_GPU);
+        auto devices_from_platform = get_devices(cl_platform_ids[i], CL_DEVICE_TYPE_GPU);
+        devices.insert(devices.end(), devices_from_platform.begin(), devices_from_platform.end());
     }
-    cl_device_id device_id = select_device(cl_platform_ids[0], CL_DEVICE_TYPE_GPU);
-    return device_id;
+
+    for (int i = 0; i < cl_platform_ids.size(); i++)
+    {
+        auto devices_from_platform = get_devices(cl_platform_ids[i], CL_DEVICE_TYPE_CPU);
+        devices.insert(devices.end(), devices_from_platform.begin(), devices_from_platform.end());
+    }
+
+    return devices[requested_index];
 }
