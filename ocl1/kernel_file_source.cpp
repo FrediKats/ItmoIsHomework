71a8affcad7543e4010756e25e42afb41c6bc49a
Fredi Kats <fredikats@outlook.com>
2021-10-09 20:54:34 +0300

    feat: add kernel source

---
+++ ocl1/kernel_file_source.cpp
@@ -0,0 +1,25 @@
+﻿#include "kernel_file_source.h"
+
+#include <fstream>
+#include <utility>
+
+kernel_file_source::kernel_file_source(std::string file_path): file_path_(std::move(file_path))
+{
+}
+
+//NB: https://stackoverflow.com/a/28344440
+std::string kernel_file_source::get_kernel_source_code()
+{
+	std::ifstream ifs("kernel.txt");
+	try
+	{
+		auto result = std::string((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));;
+		ifs.close();
+		return result;
+	}
+	catch (...)
+	{
+		ifs.close();
+		throw;
+	}
+}
---
+++ ocl1/kernel_file_source.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "kernel_source.h"
+
+class kernel_file_source : public kernel_source
+{
+public:
+	explicit kernel_file_source(std::string file_path);
+
+	std::string get_kernel_source_code() override;
+private:
+	std::string file_path_;
+};
