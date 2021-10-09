dffd0bde4200db7b892e1d84d789def6094e6b33
Fredi Kats <fredikats@outlook.com>
2021-10-09 22:51:29 +0300

    feat: add execution context

---
+++ ocl1/execution_context.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "device.h"
+
+class execution_context
+{
+public:
+	explicit execution_context(device device);
+
+	cl_context context;
+	cl_command_queue command_queue;
+};
---
+++ ocl1/program_builder.cpp
@@ -0,0 +1,20 @@
+﻿#include "program_builder.h"
+
+cl_program program_builder::build(cl_context context, std::string kernel_source_code)
+{
+    const char* kernel_source_code_link = kernel_source_code.c_str();
+    int err;
+
+    //TODO: prebuild
+    cl_program program = clCreateProgramWithSource(context, 1, &kernel_source_code_link, nullptr, &err);
+    if (!program)
+	    throw std::exception("Error: Failed to create compute program");
+
+    //TODO: handle error
+    //TODO: clGetProgramBuildLog
+    err = clBuildProgram(program, 0, nullptr, nullptr, nullptr, nullptr);
+    if (!program)
+        throw std::exception("Error: Failed to create compute program");
+
+    return program;
+}
