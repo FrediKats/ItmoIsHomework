3711df6e8d04fa45516ec01ed4306729210094d5
Fredi Kats <fredikats@outlook.com>
2021-10-10 13:12:15 +0300

    feat: add error handling in some places

---
+++ ocl1/ocl_exception.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <exception>
+#include <string>
+
+class ocl_exception final : public std::exception
+{
+public:
+	explicit ocl_exception(const std::string& message) : exception(message.c_str())
+	{
+	}
+};
--- ocl1/sum_kernel_response.cpp
+++ ocl1/sum_kernel_response.cpp
@@ -1,21 +1,26 @@
 ﻿#include "sum_kernel_response.h"
 
+#include "error_message.h"
+#include "error_validator.h"
+
 sum_kernel_response::sum_kernel_response()
 {
 }
 
 void sum_kernel_response::setup(execution_context execution_context_instance, cl_kernel kernel)
 {
-	//TODO: error handling
 	int err;
-	cl_mem output = clCreateBuffer(execution_context_instance.context, CL_MEM_WRITE_ONLY, sizeof(int), nullptr, &err);
+	const cl_mem output = clCreateBuffer(execution_context_instance.context, CL_MEM_WRITE_ONLY, sizeof(int), nullptr, &err);
+
+	validate_error(err).or_throw(error_message::about_kernel_response_setup);
+
 	clSetKernelArg(kernel, 2, sizeof(cl_mem), &output);
 	output_memory_ = output;
 }
 
 void sum_kernel_response::read_result(execution_context execution_context_instance)
 {
-	//TODO: error handling
 	int err;
 	err = clEnqueueReadBuffer(execution_context_instance.command_queue, output_memory_, CL_TRUE, 0, sizeof(int), &c, 0, nullptr, nullptr);
+	validate_error(err).or_throw(error_message::about_kernel_response_read);
 }
