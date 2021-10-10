6bfab8800854f90ed2b65f5479630ba80104593e
Fredi Kats <fredikats@outlook.com>
2021-10-10 20:58:08 +0300

    fix: cl allocation size

--- ocl1/multiplication_kernel_argument.cpp
+++ ocl1/multiplication_kernel_argument.cpp
@@ -13,23 +13,25 @@ multiplication_kernel_argument::multiplication_kernel_argument(int n, int k, int
 
 void multiplication_kernel_argument::write_arguments(execution_context execution_context_instance, cl_kernel kernel)
 {
-	int matrix_a_size = k_ * m_;
-	int matrix_b_size = n_ * k_;
+	unsigned long long matrix_a_size = sizeof(float) * k_ * m_;
+	unsigned long long matrix_b_size = sizeof(float) * n_ * k_;
 
 	int width_a = k_;
 	int width_b = n_;
 
 	int err;
-	cl_mem matrix_a_input = clCreateBuffer(execution_context_instance.context, CL_MEM_READ_ONLY, sizeof(int) * matrix_a_size, nullptr, &err);
+	cl_mem matrix_a_input = clCreateBuffer(execution_context_instance.context, CL_MEM_READ_ONLY, matrix_a_size, nullptr, &err);
 	validate_error(err).or_throw(error_message::about_kernel_argument_create_buffer);
-	err = clEnqueueWriteBuffer(execution_context_instance.command_queue, matrix_a_input, CL_TRUE, 0, sizeof(int) * matrix_a_size, &matrix_a_, 0, nullptr, nullptr);
+	err = clEnqueueWriteBuffer(execution_context_instance.command_queue, matrix_a_input, CL_TRUE, 0, matrix_a_size, &matrix_a_, 0, nullptr, nullptr);
 	validate_error(err).or_throw(error_message::about_kernel_argument_write);
 	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &matrix_a_input);
+
+	//NB: CL_INVALID_ARG_SIZE -51
 	validate_error(err).or_throw(error_message::about_set_kernel_argument);
 
-	cl_mem matrix_b_input = clCreateBuffer(execution_context_instance.context, CL_MEM_READ_ONLY, sizeof(int) * matrix_b_size, nullptr, &err);
+	cl_mem matrix_b_input = clCreateBuffer(execution_context_instance.context, CL_MEM_READ_ONLY, matrix_b_size, nullptr, &err);
 	validate_error(err).or_throw(error_message::about_kernel_argument_create_buffer);
-	err = clEnqueueWriteBuffer(execution_context_instance.command_queue, matrix_b_input, CL_TRUE, 0, sizeof(int) * matrix_b_size, &matrix_b_, 0, nullptr, nullptr);
+	err = clEnqueueWriteBuffer(execution_context_instance.command_queue, matrix_b_input, CL_TRUE, 0, matrix_b_size, &matrix_b_, 0, nullptr, nullptr);
 	validate_error(err).or_throw(error_message::about_kernel_argument_write);
 	err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &matrix_b_input);
 	validate_error(err).or_throw(error_message::about_set_kernel_argument);
--- ocl1/multiplication_kernel_response.cpp
+++ ocl1/multiplication_kernel_response.cpp
@@ -9,8 +9,10 @@ multiplication_kernel_response::multiplication_kernel_response(size_t size, floa
 
 void multiplication_kernel_response::setup(execution_context execution_context_instance, cl_kernel kernel)
 {
+	auto allocate_size = sizeof(float) * size;
+
 	int err;
-	cl_mem output = clCreateBuffer(execution_context_instance.context, CL_MEM_WRITE_ONLY, size, nullptr, &err);
+	cl_mem output = clCreateBuffer(execution_context_instance.context, CL_MEM_WRITE_ONLY, allocate_size, nullptr, &err);
 	validate_error(err).or_throw(error_message::about_kernel_response_setup);
 
 	clSetKernelArg(kernel, 4, sizeof(cl_mem), &output);
@@ -20,7 +22,9 @@ void multiplication_kernel_response::setup(execution_context execution_context_i
 
 void multiplication_kernel_response::read_result(execution_context execution_context_instance)
 {
+	auto allocate_size = sizeof(float) * size;
+
 	int err;
-	err = clEnqueueReadBuffer(execution_context_instance.command_queue, output_memory_, CL_TRUE, 0, size, result, 0, nullptr, nullptr);
+	err = clEnqueueReadBuffer(execution_context_instance.command_queue, output_memory_, CL_TRUE, 0, allocate_size, result, 0, nullptr, nullptr);
 	validate_error(err).or_throw(error_message::about_kernel_response_read);
 }
