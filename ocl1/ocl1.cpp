a2919db340cf910c9acf6b6f954a2e8e31f8e50b
Fredi Kats <fredikats@outlook.com>
2021-10-10 19:42:58 +0300

    feat: add multiplication kernel

--- ocl1/ocl1.cpp
+++ ocl1/ocl1.cpp
@@ -5,11 +5,13 @@
 #include "cl_device_provider.h"
 #include "execution_context.h"
 #include "kernel_file_source.h"
+#include "multiplication_kernel.h"
+#include "multiplication_kernel_argument.h"
+#include "multiplication_kernel_response.h"
 #include "sum_kernel.h"
 #include "sum_kernel_argument.h"
 
-
-int main()
+void execute_sum()
 {
     cl_device_provider device_provider = cl_device_provider();
     //TODO: read index from args
@@ -30,6 +32,34 @@ int main()
     //TODO: add sync | async
     //cl_int write_byte_count = clEnqueueWriteBuffer(command_queue, buffer, false, 0, sizeof(cl_int), nullptr, 0, nullptr, nullptr);
     //cl_int read_byte_count = clEnqueueReadBuffer(command_queue, buffer, false, 0, sizeof(cl_int), nullptr, 0, nullptr, nullptr);
+}
+
+void execute_mult()
+{
+    cl_device_provider device_provider = cl_device_provider();
+    //TODO: read index from args
+    device device = device_provider.select_device(0);
+    execution_context execution_context_instance = execution_context(device);
+
+    kernel_file_source kernel_source = kernel_file_source("multiplication.txt");
+
+    float* a = static_cast<float*>(malloc(sizeof(float)));
+    float* b = static_cast<float*>(malloc(sizeof(float)));
+    a[0] = 2;
+    b[0] = 3;
+    float* c = static_cast<float*>(malloc(sizeof(float)));
+
+    multiplication_kernel_argument argument = multiplication_kernel_argument(1, 1, 1, a, b);
+    multiplication_kernel_response response = multiplication_kernel_response(1, c);
+    multiplication_kernel kernel = create_multiplication_kernel(kernel_source, execution_context_instance);
+    kernel.execute(argument, response);
+    std::cout << c[0];
+    return;
+}
+
+int main()
+{
+    execute_mult();
 
 	return 0;
 }
