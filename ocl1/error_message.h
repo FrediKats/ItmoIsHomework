35d0ce3290aea24f3a1934405c0090fa56fce922
Fredi Kats <fredikats@outlook.com>
2021-10-10 12:52:08 +0300

    feat: add fluent error validator

---
+++ ocl1/error_message.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <string>
+
+enum error_message
+{
+	about_kernel_response_setup,
+};
+
+inline std::string error_message_to_string(const error_message message)
+{
+	switch (message)
+	{
+	case about_kernel_response_setup:
+		return "Error while setup kernel response";
+
+	default:
+		throw std::exception(("Unexpected error message type: " + std::to_string(message)).c_str());
+	}
+}
