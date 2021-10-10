76745f83c553581e8ecd64c3195993b564837b5d
Fredi Kats <fredikats@outlook.com>
2021-10-10 12:52:08 +0300

    feat: add fluent error validator

---
+++ ocl1/error_validator.cpp
@@ -0,0 +1,18 @@
+﻿#include "error_validator.h"
+
+error_validator::error_validator(const bool is_success): is_success(is_success)
+{
+}
+
+void error_validator::or_throw(const std::string& message) const
+{
+	if (is_success)
+		return;
+
+	throw std::exception(message.c_str());
+}
+
+void error_validator::or_throw(const error_message message) const
+{
+	or_throw(error_message_to_string(message));
+}
