5d1685d5b9c7d7579dd873bdbb64e2e43a99adef
Fredi Kats <fredikats@outlook.com>
2021-10-10 19:42:58 +0300

    feat: add multiplication kernel

---
+++ ocl1/multiplication_kernel.cpp
@@ -0,0 +1,1 @@
+﻿#include "multiplication_kernel.h"
---
+++ ocl1/multiplication_kernel.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "error_validator.h"
+#include "kernel_contract.h"
+#include "kernel_source.h"
+#include "multiplication_kernel_argument.h"
+#include "multiplication_kernel_response.h"
+#include "program_builder.h"
+
+class multiplication_kernel : public kernel_contract<multiplication_kernel_argument, multiplication_kernel_response>
+{
+public:
+	multiplication_kernel(const execution_context& execution_context_instance, cl_kernel kernel)
+		: kernel_contract<multiplication_kernel_argument, multiplication_kernel_response>(
+			execution_context_instance, kernel)
+	{
+	}
+};
+
+
+inline multiplication_kernel create_multiplication_kernel(kernel_source& source, const execution_context execution_context_instance)
+{
+    const std::string kernel_source_code = source.get_kernel_source_code();
+    program_builder builder = program_builder();
+    const cl_program program = builder.build(execution_context_instance.context, kernel_source_code);
+
+    int err;
+    cl_kernel kernel = clCreateKernel(program, "simpleMultiply", &err);
+    validate_error(err).or_throw(error_message::about_create_kernel);
+
+    return multiplication_kernel(execution_context_instance, kernel);
+}
