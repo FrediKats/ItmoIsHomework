0181167dc9b8ee7bdc395729298b738ed504f2de
Fredi Kats <fredikats@outlook.com>
2021-10-10 19:16:11 +0300

    fix: remove hardcoded path to kernel

--- ocl1/kernel_file_source.cpp
+++ ocl1/kernel_file_source.cpp
@@ -10,7 +10,7 @@ kernel_file_source::kernel_file_source(std::string file_path): file_path_(std::m
 //NB: https://stackoverflow.com/a/28344440
 std::string kernel_file_source::get_kernel_source_code()
 {
-	std::ifstream ifs("kernel.txt");
+	std::ifstream ifs(file_path_);
 	try
 	{
 		auto result = std::string((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));;
