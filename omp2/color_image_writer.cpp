4faaa4d36cd864d71275bd69539b05bc10bee291
Fredi Kats <fredikats@outlook.com>
2021-10-14 20:48:48 +0300

    fix: correct read and write for P5

--- omp2/color_image_writer.cpp
+++ omp2/color_image_writer.cpp
@@ -20,23 +20,47 @@ void omp2::color_image_writer::write(const pnm_image_descriptor<color>& image) c
 		if (argument_count < 0)
 			throw std::exception(("Failed to write arguments to file. Return: " + std::to_string(argument_count)).c_str());
 
-		const size_t buffer_size = 3 * image.width * image.height;
-		pixel_data = new unsigned char[buffer_size];
-
-		for (size_t index = 0; index < image.color.size(); index++)
+		if (image.version == 6)
 		{
-			const auto current_pixel = image.color[index];
-			pixel_data[index * 3] = current_pixel.red;
-			pixel_data[index * 3 + 1] = current_pixel.green;
-			pixel_data[index * 3 + 2] = current_pixel.blue;
+			const size_t buffer_size = 3 * image.width * image.height;
+			pixel_data = new unsigned char[buffer_size];
+
+			for (size_t index = 0; index < image.color.size(); index++)
+			{
+				const auto current_pixel = image.color[index];
+				pixel_data[index * 3] = current_pixel.red;
+				pixel_data[index * 3 + 1] = current_pixel.green;
+				pixel_data[index * 3 + 2] = current_pixel.blue;
+			}
+
+			const size_t pixel_count = fwrite(pixel_data, sizeof(char), buffer_size, file);
+			if (pixel_count != buffer_size)
+				throw std::exception("Failed to write pixels to file");
+
+			delete[] pixel_data;
+			fclose(file);
+		}
+		else
+		{
+			const size_t buffer_size = image.width * image.height;
+			pixel_data = new unsigned char[buffer_size];
+
+			for (size_t index = 0; index < image.width * image.height; index++)
+			{
+				const auto current_pixel = image.color[index];
+				pixel_data[index] = current_pixel.red;
+			}
+
+			const size_t pixel_count = fwrite(pixel_data, sizeof(char), buffer_size, file);
+			if (pixel_count != buffer_size)
+				throw std::exception("Failed to write pixels to file");
+
+			delete[] pixel_data;
+			fclose(file);
 		}
 
-		const size_t pixel_count = fwrite(pixel_data, sizeof(char), buffer_size, file);
-		if (pixel_count != buffer_size)
-			throw std::exception("Failed to write pixels to file");
 
-		delete[] pixel_data;
-		fclose(file);
+		
 	}
 	catch (...)
 	{
