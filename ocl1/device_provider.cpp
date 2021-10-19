c2ee9d773cce1027b3bee12cfe0d1e15154e2f73
Fredi Kats <fredikats@outlook.com>
2021-10-19 22:13:48 +0300

    feat: add model for platform info

--- ocl1/device_provider.cpp
+++ ocl1/device_provider.cpp
@@ -5,49 +5,31 @@
 #include <vector>
 
 #include "error_validator.h"
+#include "platform_info.h"
 
 namespace ocl1
 {
-	std::vector<cl_platform_id> get_platforms(const bool trace_detailed_info)
+	std::vector<platform_info> get_platforms(const bool trace_detailed_info)
 	{
 		cl_uint num_platforms;
 		cl_int err = clGetPlatformIDs(0, nullptr, &num_platforms);
 		validate_error(err).or_throw(about_create_device_group);
 
 		//TODO: clean resources
-		auto cl_selected_platform_id = static_cast<cl_platform_id*>(malloc(sizeof(cl_platform_id) * num_platforms));
+		const auto cl_selected_platform_id = new cl_platform_id[num_platforms];
 		err = clGetPlatformIDs(num_platforms, cl_selected_platform_id, nullptr);
 		validate_error(err).or_throw(about_create_device_group);
 
 		// NB: https://stackoverflow.com/a/25926679
 		//TODO: add flag from ctor for extended logs
-		const char* attribute_names[5] = { "Name", "Vendor", "Version", "Profile", "Extensions" };
-		constexpr cl_platform_info attribute_types[5] = {
-			CL_PLATFORM_NAME,
-			CL_PLATFORM_VENDOR,
-			CL_PLATFORM_VERSION,
-			CL_PLATFORM_PROFILE,
-			CL_PLATFORM_EXTENSIONS
-		};
-
-		auto result = std::vector<cl_platform_id>(num_platforms);
+		auto result = std::vector<platform_info>(num_platforms);
 		for (cl_uint i = 0; i < num_platforms; i++)
 		{
-			for (int j = 0; j < 5; j++)
-			{
-				//TODO: add error validation
-				size_t info_size;
-				clGetPlatformInfo(cl_selected_platform_id[i], attribute_types[j], 0, nullptr, &info_size);
-				//TODO: clean resources
-				const auto info = static_cast<char*>(malloc(info_size));
+			platform_info platform(cl_selected_platform_id[i]);
+			if (trace_detailed_info)
+				std::cout << platform.to_string();
 
-				clGetPlatformInfo(cl_selected_platform_id[i], attribute_types[j], info_size, info, nullptr);
-
-				if (trace_detailed_info)
-					printf("  %d.%d %-11s: %s\n", i + 1, j + 1, attribute_names[j], info);
-
-				result[i] = cl_selected_platform_id[i];
-			}
+			result[i] = platform;
 		}
 
 		return result;
@@ -55,10 +37,10 @@ namespace ocl1
 
 	//NB: https://www.khronos.org/registry/OpenCL/specs/opencl-1.2.pdf
 	// CL_DEVICE_HOST_UNIFIED_MEMORY cl_bool Is CL_TRUE if the device and the host have a unified memory subsystemand is CL_FALSE otherwise
-	std::vector<device> get_devices(cl_platform_id platform_id, cl_device_type device_type, bool use_unified_memory_subsystem, const bool trace_detailed_info)
+	std::vector<device> get_devices(platform_info platform, cl_device_type device_type, bool use_unified_memory_subsystem, const bool trace_detailed_info)
 	{
 		cl_uint device_count;
-		cl_int err = clGetDeviceIDs(platform_id, device_type, 0, nullptr, &device_count);
+		cl_int err = clGetDeviceIDs(platform.id, device_type, 0, nullptr, &device_count);
 
 		// NB: in some case we request CPU-type devices from GPU devices 
 		if (err == CL_DEVICE_NOT_FOUND)
@@ -68,7 +50,7 @@ namespace ocl1
 
 		//TODO: clean resources
 		auto device_ids = static_cast<cl_device_id*>(malloc(sizeof(cl_device_id) * device_count));
-		err = clGetDeviceIDs(platform_id, device_type, device_count, device_ids, &device_count);
+		err = clGetDeviceIDs(platform.id, device_type, device_count, device_ids, &device_count);
 		validate_error(err).or_throw(about_getting_platform_devices);
 
 		auto result = std::vector<device>(device_count);
@@ -105,7 +87,7 @@ namespace ocl1
 	device device_provider::select_device(const int requested_index, const bool trace_detailed_info)
 	{
 		auto devices = std::vector<device>();
-		const auto cl_platform_ids = get_platforms(trace_detailed_info);
+		const std::vector<platform_info> cl_platform_ids = get_platforms(trace_detailed_info);
 
 		for (size_t i = 0; i < cl_platform_ids.size(); i++)
 		{
