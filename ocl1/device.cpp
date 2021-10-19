475c0cad1f2662e2aa8edd481ac314222a3ed38d
Fredi Kats <fredikats@outlook.com>
2021-10-19 22:36:21 +0300

    refactor: encapsulate device creation logic

--- ocl1/device.cpp
+++ ocl1/device.cpp
@@ -1,6 +1,7 @@
 ﻿#include "device.h"
 
-#include <utility>
+#include "error_validator.h"
+#include "ocl_exception.h"
 
 namespace ocl1
 {
@@ -8,10 +9,56 @@ namespace ocl1
 	{
 	}
 
-	device::device(cl_device_id id, std::string name, const bool is_unified_memory_subsystem):
-		id(id),
-		name(std::move(name)),
-		is_unified_memory_subsystem(is_unified_memory_subsystem)
+	device::device(cl_device_id id)
 	{
+		name = get_value(CL_DEVICE_NAME);
+		is_unified_memory_subsystem = get_bool_value(CL_DEVICE_HOST_UNIFIED_MEMORY);
+	}
+
+	std::string device::to_string() const
+	{
+		return "Device" + name + " (" + "is_unified_memory_subsystem: " + std::to_string(is_unified_memory_subsystem) + ")";
+	}
+
+	std::string device::get_value(cl_platform_info device_info_type) const
+	{
+		char* info = nullptr;
+		try
+		{
+			size_t return_size;
+			int err = clGetDeviceInfo(id, device_info_type, 0, nullptr, &return_size);
+			validate_error(err).or_throw(about_getting_device_info);
+			info = new char[return_size];
+			err = clGetDeviceInfo(id, device_info_type, return_size, info, &return_size);
+			validate_error(err).or_throw(about_getting_device_info);
+
+			auto result = std::string(info);
+			delete info;
+			return result;
+		}
+		catch (...)
+		{
+			delete info;
+			throw;
+		}
+	}
+
+	bool device::get_bool_value(cl_platform_info device_info_type) const
+	{
+		//TODO: fix
+		return false;
+
+		size_t return_size;
+		int err = clGetDeviceInfo(id, device_info_type, 0, nullptr, &return_size);
+		validate_error(err).or_throw(about_getting_device_info);
+
+		if (return_size != 4)
+			throw ocl_exception("Unexpected bool size" + std::to_string(return_size));
+
+		bool result = false;
+		err = clGetDeviceInfo(id, device_info_type, return_size, &result, &return_size);
+		validate_error(err).or_throw(about_getting_device_info);
+
+		return result;
 	}
 }
