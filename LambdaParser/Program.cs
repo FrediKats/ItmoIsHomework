39ae1b35f189066630aa3cacee0e0a3fd4b71f74
Fredi Kats <fredikats@outlook.com>
2022-04-16 12:59:01 +0300

    feat: add some syntax node

--- LambdaParser/Program.cs
+++ LambdaParser/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Immutable;
+
 const char Lambda = 'λ';
 
 Console.WriteLine("Hello, World!");
 
 var λnΛfΛxFNFX = "λn.λf.λx.f (n f x)";
+var λfΛxFFXSqr = "(((λf . (λx . (f (f x)))) sqr) 3)";
 
 
-
-void Parse(Span<char> expression)
+LambdaSyntaxNode Parse(Span<char> expression)
 {
     switch (expression[0])
     {
@@ -15,7 +18,11 @@ void Parse(Span<char> expression)
             throw new NotImplementedException();
         case '(':
             throw new NotImplementedException();
+        case ')':
+            throw new NotImplementedException();
     }
+
+    throw new NotImplementedException();
 }
 
 public class NodeLocation
@@ -41,6 +48,47 @@ public abstract class LambdaSyntaxNode
     protected LambdaSyntaxNode(NodeLocation location)
     {
         Location = location;
+    }
+}
+
+public abstract class ExpressionLambdaSyntaxNode : LambdaSyntaxNode
+{
+    protected ExpressionLambdaSyntaxNode(NodeLocation location) : base(location)
+    {
+    }
+}
+
+public class LetterLambdaSyntaxNode : ExpressionLambdaSyntaxNode
+{
+    public string Value { get; }
+
+    public LetterLambdaSyntaxNode(NodeLocation location, string value) : base(location)
+    {
+        Value = value;
+    }
+}
+
+public class FuncInvokeSyntaxNode : ExpressionLambdaSyntaxNode
+{
+    public LetterLambdaSyntaxNode FunctionName { get; }
+    public ImmutableArray<ExpressionLambdaSyntaxNode> Arguments { get; }
+
+    public FuncInvokeSyntaxNode(NodeLocation location, LetterLambdaSyntaxNode functionName, ImmutableArray<ExpressionLambdaSyntaxNode> arguments) : base(location)
+    {
+        FunctionName = functionName;
+        Arguments = arguments;
+    }
+}
+
+public class AbstractionLambdaSyntaxNode : LambdaSyntaxNode
+{
+    public LetterLambdaSyntaxNode Argument { get; }
+    public LambdaSyntaxNode Body { get; }
+
+    public AbstractionLambdaSyntaxNode(NodeLocation location, LetterLambdaSyntaxNode argument, LambdaSyntaxNode body) : base(location)
+    {
+        Argument = argument;
+        Body = body;
     }
 }
 
